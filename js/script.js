55f2929da749c41aa23617ce20f5f3d214831dd3
pxseu <57842793+pxseu@users.noreply.github.com>
2019-11-30 18:58:15 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-function  click1(){
-alert("S U C C");
-window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-}
-function  click2(){
-alert("S U C C");
-window.location.href = "https://modman276.github.io/ZeroTwo/index.html";
-}
 $('.btn').click(function(){
   $(".box").toggleClass("box-show");
 });
