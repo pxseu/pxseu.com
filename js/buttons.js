1c749b9330084117a42b6c0725f10e7fe866b783
pxseu <57842793+pxseu@users.noreply.github.com>
2019-11-30 19:00:28 +0100

    Update buttons.js

--- js/buttons.js
+++ js/buttons.js
@@ -1,8 +1,6 @@
 function click1(){
-alert("S U C C");
 window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 }
 function click2(){
-alert("S U C C");
 window.location.href = "https://modman276.github.io/ZeroTwo/index.html";
 }
