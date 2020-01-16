a18959ac39b63dc19e80a9ab99c6e762a2d12613
Modamn276 <57842793+Modman276@users.noreply.github.com>
2020-01-16 20:27:21 +0100

    Update Title 2.1.0

--- Index/js/buttons.js
+++ Index/js/buttons.js
@@ -1,6 +1,9 @@
 function click1(){
-window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 }
 function click2(){
-window.location.href = "https://modman276.github.io/ZeroTwo/index.html";
+    window.location.href = "https://modman276.github.io/ZeroTwo/index.html";
+}
+function click3(){
+    Array.from(document.querySelectorAll("ul")).forEach(el => el.classList.toggle("show"));
 }
