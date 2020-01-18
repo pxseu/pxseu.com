7b52274d155fb3ea2d843eddc6451f7905e43e6e
pxseu <57842793+pxseu@users.noreply.github.com>
2020-01-18 17:55:29 +0100

    Update Title 2.2.0
    -added restricted words for the user to not breake the webpage

--- Index/js/script.js
+++ Index/js/script.js
@@ -2,16 +2,29 @@ $('.btn').click(function(){
   $(".box").toggleClass("box-show");
 });
 
-//to have a preview of color
 $('#hex').keyup(function(){
   $('.prev').css("background",$('#hex').val());
 });
 
-//to apply color
 $('.apply').click(function(){
+  var restrictedWords = new Array("white","rgb(255,255,255)","#ffffff","rgba(255,255,255,1)","rgba(0,0,0,0)");
+  var txtInput = document.getElementById("hex").value;
+  var error = 0;
+  for (var i = 0; i < restrictedWords.length; i++) {
+      var val = restrictedWords[i];
+      if ((txtInput.toLowerCase()).indexOf(val.toString()) > -1) {
+          error = error + 1;
+      }
+  }
+
+  if (error > 0) {
+      alert("Wybranie koloru białego może utrudnić prawidłowe używanie strony!")
+  }
+  else {
   $('body').css("background",$('#hex').val());
   $('.text1').css("background",$('#hex').val());
   $('.button1:active').css("background-color",$('#hex').val());
   $('li a:hover').css("background-color",$('#hex').val());
     $(".box").toggleClass("box-show");
+  }
 })
