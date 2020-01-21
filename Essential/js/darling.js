ff83edf9a4e5c65751d6b85b073142554ce3632f
Modamn276 <57842793+Modman276@users.noreply.github.com>
2020-01-21 20:22:57 +0100

    bug fix 1.3

--- Essential/js/darling.js
+++ Essential/js/darling.js
@@ -30,37 +30,42 @@ document.addEventListener('keydown', function(e) {
 function darlingCodePassed() {
   var audio2 = new Audio('Essential/media/darling.mp3');
   audio2.play();
-
   audio2.onended=function(){
-    Array.from(document.querySelectorAll("body")).forEach(el => el.classList.add("darling"));
-    Array.from(document.querySelectorAll("body")).forEach(el => el.classList.remove("konami"));
-    Array.from(document.querySelectorAll(".text1")).forEach(el => el.classList.add("transparent"));
-    Array.from(document.querySelectorAll(".text2")).forEach(el => el.classList.add("pink"));
-    Array.from(document.querySelectorAll("p a strong")).forEach(el => el.classList.add("pink"));
-    Array.from(document.querySelectorAll(".text2 strong")).forEach(el => el.classList.add("pink"));
-    Array.from(document.querySelectorAll("button")).forEach(el => el.classList.add("pink"));
-    Array.from(document.querySelectorAll(".btn")).forEach(el => el.classList.add("pink"));
-    Array.from(document.querySelectorAll(".menubtn")).forEach(el => el.classList.add("pink"));
-    Array.from(document.querySelectorAll("li a")).forEach(el => el.classList.add("pink"));
-    Array.from(document.querySelectorAll(".box h2")).forEach(el => el.classList.add("pink"));
-    Array.from(document.querySelectorAll(".apply")).forEach(el => el.classList.add("pink"));
-    document.getElementById( "clickmebtn" ).setAttribute( "onClick", "ditfx()" );    
+    pinker();
     alert("Darling I found you!");
  }
 }
+
+
 var audioditfx = new Audio('Essential/media/ditfx.mp3');
 
 function ditfx(){
     audioditfx.play();
     audioditfx.volume = 0.3;
-    document.getElementById( "clickmebtn" ).setAttribute( "onClick", "ditfxpause()" ); 
+    document.getElementById( "clickmebtn" ).setAttribute( "onClick", "ditfxpause()" );
     document.getElementById( "clickmebtn" ).innerHTML = "Darling!";
 }
 function ditfxpause(){
     audioditfx.pause();
-    document.getElementById( "clickmebtn" ).setAttribute( "onClick", "ditfxunpause()" );  
+    document.getElementById( "clickmebtn" ).setAttribute( "onClick", "ditfxunpause()" );
 }
 function ditfxunpause(){
     audioditfx.play();
     document.getElementById( "clickmebtn" ).setAttribute( "onClick", "ditfxpause()" );
+}
+
+function pinker(){
+  Array.from(document.querySelectorAll("body")).forEach(el => el.classList.add("darling"));
+  Array.from(document.querySelectorAll("body")).forEach(el => el.classList.remove("konami"));
+  Array.from(document.querySelectorAll(".text1")).forEach(el => el.classList.add("transparent"));
+  Array.from(document.querySelectorAll(".text2")).forEach(el => el.classList.add("pink"));
+  Array.from(document.querySelectorAll("p a strong")).forEach(el => el.classList.add("pink"));
+  Array.from(document.querySelectorAll("h1 strong")).forEach(el => el.classList.add("pink"));
+  Array.from(document.querySelectorAll("button")).forEach(el => el.classList.add("pink"));
+  Array.from(document.querySelectorAll(".btn")).forEach(el => el.classList.add("pink"));
+  Array.from(document.querySelectorAll(".menubtn")).forEach(el => el.classList.add("pink"));
+  Array.from(document.querySelectorAll("li a")).forEach(el => el.classList.add("pink"));
+  Array.from(document.querySelectorAll(".box h2")).forEach(el => el.classList.add("pink"));
+  Array.from(document.querySelectorAll(".apply")).forEach(el => el.classList.add("pink"));
+  document.getElementById( "clickmebtn" ).setAttribute( "onClick", "ditfx()" );
 }
