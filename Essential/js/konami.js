f5ef726c77efdc0d5d8847c6edc01aac582d7d07
pxseu <57842793+pxseu@users.noreply.github.com>
2020-01-21 14:54:02 +0100

    Update Title 4.0.0

--- Essential/js/konami.js
+++ Essential/js/konami.js
@@ -35,7 +35,9 @@ function konamiCodePassed() {
     Array.from(document.querySelectorAll("body")).forEach(el => el.classList.remove("darling"));
     Array.from(document.querySelectorAll(".text1")).forEach(el => el.classList.add("transparent"));
     Array.from(document.querySelectorAll(".text2")).forEach(el => el.classList.add("pink"));
-    Array.from(document.querySelectorAll(".button1")).forEach(el => el.classList.add("pink"));
+    Array.from(document.querySelectorAll("p a strong")).forEach(el => el.classList.add("pink"));
+    Array.from(document.querySelectorAll("h1 strong")).forEach(el => el.classList.add("pink"));
+    Array.from(document.querySelectorAll("button")).forEach(el => el.classList.add("pink"));        
     Array.from(document.querySelectorAll(".btn")).forEach(el => el.classList.add("pink"));
     Array.from(document.querySelectorAll(".menubtn")).forEach(el => el.classList.add("pink"));
     Array.from(document.querySelectorAll("li a")).forEach(el => el.classList.add("pink"));
