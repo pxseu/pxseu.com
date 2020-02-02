5deef4116d1846f9baf4f1d4bcc0b5318f813228
pxseu <57842793+pxseu@users.noreply.github.com>
2020-02-02 12:48:26 +0100

    Add files via upload

--- Essential/js/konami.js
+++ Essential/js/konami.js
@@ -1,3 +1,71 @@
 //copyright@modman276.github.io  all rights reserved
+var konamiKeys = {
+  37: 'left',
+  38: 'up',
+  39: 'right',
+  40: 'down',
+  65: 'a',
+  66: 'b'
+};
+var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+var konamiCodePosition = 0;
 
-var konamiKeys={37:"left",38:"up",39:"right",40:"down",65:"a",66:"b"},konamiCode=["up","up","down","down","left","right","left","right","b","a"],konamiCodePosition=0;function konamiCodePassed(){var e=new Audio("Essential/media/cheats.mp3");e.play(),e.onended=function(){if($("body").css("background-image",""),Array.from(document.querySelectorAll("body")).forEach(e=>e.classList.add("konami")),Array.from(document.querySelectorAll("body")).forEach(e=>e.classList.remove("darling")),Array.from(document.querySelectorAll("body")).forEach(e=>e.classList.remove("monika")),Array.from(document.querySelectorAll(".text1")).forEach(e=>e.classList.add("transparent")),Array.from(document.querySelectorAll(".text2, .email, .discord, h1 strong, button, .btn, .menubtn, li a, .box h2, .apply, .clock, .box")).forEach(e=>e.classList.add("pink")),alert("Cheats Activated"),$("#clickmebtn").attr("onClick","dvabtn()"),$("#clickmebtn").innerHTML="D.Va",$("#gifbox").remove(),$("#clickmebtn").length){var e=document.createElement("IMG");e.id="gifbox",e.src="Essential/media/dva_static.gif",e.setAttribute("onClick","dva_change()"),e.classList.add("gifbox"),document.body.appendChild(e)}}}function dvabtn(){window.location.replace("https://www.youtube.com/embed/fDwItecEzFk")}function dva_change(){var e=document.getElementById("gifbox");e.src="Essential/media/dva.gif",e.setAttribute("onClick",""),e.classList.add("pinkshadow")}document.addEventListener("keydown",function(e){konamiKeys[e.keyCode]==konamiCode[konamiCodePosition]?++konamiCodePosition==konamiCode.length&&(konamiCodePassed(),konamiCodePosition=0):konamiCodePosition=0});
+document.addEventListener('keydown', function(e) {
+  var key = konamiKeys[e.keyCode];
+  var requiredKey = konamiCode[konamiCodePosition];
+
+  if (key == requiredKey) {
+
+    konamiCodePosition++;
+
+    if (konamiCodePosition == konamiCode.length) {
+      konamiCodePassed();
+      konamiCodePosition = 0;
+    }
+  } else {
+    konamiCodePosition = 0;
+  }
+});
+
+function konamiCodePassed() {
+  var audio = new Audio('Essential/media/cheats.mp3');
+  audio.play();
+
+  audio.onended=function(){
+    $('body').css("background-image", "");
+    if ($(window).width() < 700) {
+      Array.from(document.querySelectorAll("body")).forEach(el => el.classList.add("konamim"));
+      Array.from(document.querySelectorAll("body")).forEach(el => el.classList.remove("konami"));
+    }
+    else { 
+      Array.from(document.querySelectorAll("body")).forEach(el => el.classList.add("konami"));
+      Array.from(document.querySelectorAll("body")).forEach(el => el.classList.remove("konamim"));
+    }
+    Array.from(document.querySelectorAll("body")).forEach(el => el.classList.remove("darling","darlingm","monika"));
+    Array.from(document.querySelectorAll("body")).forEach(el => el.classList.remove("monika"));
+    Array.from(document.querySelectorAll(".text1")).forEach(el => el.classList.add("transparent"));
+    Array.from(document.querySelectorAll(".text2, .email, .discord, h1 strong, button, .btn, .menubtn, li a, .box h2, .apply, .clock, .box")).forEach(el => el.classList.add("pink"));
+    alert("Cheats Activated");
+    $('#clickmebtn').attr("onClick", "dvabtn()");
+    $('#clickmebtn').innerHTML = "D.Va";  
+    $('#gifbox').remove();
+    if (($('#clickmebtn').length)&&($(window).width() > 600)) {
+  	var gifmake = document.createElement("IMG");
+  	gifmake.id = "gifbox";
+  	gifmake.src='Essential/media/dva_static.gif';
+  	gifmake.setAttribute( "onClick", "dva_change()" );
+  	gifmake.classList.add("gifbox");
+  	document.body.appendChild(gifmake);
+  }
+ }
+}
+function dvabtn() {
+  window.location.replace("https://www.youtube.com/embed/fDwItecEzFk");
+}
+function dva_change(){
+  var zrtsugifbox = document.getElementById('gifbox');
+  zrtsugifbox.src='Essential/media/dva.gif';
+  zrtsugifbox.setAttribute( "onClick", "" );
+  zrtsugifbox.classList.add("pinkshadow");
+}
+
