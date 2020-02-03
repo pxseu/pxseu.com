0a774183ee3faec1ae1f9e0a3625a29af439c6ca
pxseu <57842793+pxseu@users.noreply.github.com>
2020-02-04 00:13:16 +0100

    Update Title 14.0.0

--- About/js/script.js
+++ About/js/script.js
@@ -4,3 +4,6 @@ function discord(){
 function email(){
   alert("Email: ellwartkuba@gmail.com");
 }
+function playlistgo() {
+  window.location.href = "https://open.spotify.com/playlist/7LHiEoB6EeC4V124koSFFH?si=91VuHPACRPmAJeysedAkYA"
+}
