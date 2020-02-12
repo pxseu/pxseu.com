3263632f8f3cb6388a7a8c2eebcc5851e38dfb0e
Modamn276 <57842793+Modman276@users.noreply.github.com>
2020-02-12 18:36:08 +0100

    Update

--- Essential/js/script.js
+++ Essential/js/script.js
@@ -1,6 +1,14 @@
 // copyright@modman276.github.io  all rights reserved
 
 // usfl alert(localStorage.getItem("bgimgsave"))
+
+var sheet = (function() {
+  var style = document.createElement("style");
+  style.appendChild(document.createTextNode(""));
+  document.head.appendChild(style);
+  return style.sheet;
+})();
+
 hidden();
 $('.btn').click(function(){
   $(".box").toggleClass("box-show");
@@ -13,9 +21,76 @@ function inputselect() {
   document.getElementById('hex').focus();
   document.getElementById('hex').select();
 }
+
+function colorchck() {
+  if (document.getElementById("checkbox3").checked == true){
+    $("#checkbox").prop("checked", false);
+    $("#checkbox2").prop("checked", false);
+    $('.apply').attr("onClick", "colorpicker()");
+    document.getElementById("hex").placeholder = "#000000";
+    document.getElementById("hex").defaultValue = '';
+  }
+}
 function colorpicker(){
 
-  var restrictedWords = new Array("white","rgb(255,255,255)","#ffffff","rgba(255,255,255,1)","rgba(0,0,0,0)");
+  var restrictedWords = new Array("white","rgb(255,255,255)","#ffffff","rgba(255,255,255,1)","rgba(0,0,0,0)", localStorage.getItem("uicolorsave"));
+  var txtInput = document.getElementById("hex").value;
+  var error = 0;
+  for (var i = 0; i < restrictedWords.length; i++) {
+      var val = restrictedWords[i];
+      if ((txtInput.toLowerCase()).indexOf(val.toString()) > -1) {
+          error = error + 1;
+      }
+  }
+
+  if (error > 0) {
+      alert("White Theme or thesame color as ui can brake the websites look!")
+  }
+  else {
+    localStorage.setItem("bgcolorsave", document.getElementById('hex').value);
+    localStorage.setItem("imgchecksave", "0");
+    location.reload();
+}
+}
+function menubtn(){
+    Array.from(document.querySelectorAll("ul")).forEach(el => el.classList.toggle("show"));
+}
+function hidden() {
+ $(".hidden").addClass("slowmoshow");  
+  setTimeout(function(){$(".hidden").removeClass("hidden"); },200);
+  setTimeout(function(){$(".slowmoshow").removeClass("slowmoshow"); },1300);
+}
+
+function imgchck(){
+if (document.getElementById("checkbox").checked == true){
+    $("#checkbox2").prop("checked", false);
+    $("#checkbox3").prop("checked", false);
+    $('.apply').attr("onClick", "imgshow()");
+    document.getElementById("hex").placeholder = "https://somewebsite/image.png";
+    document.getElementById("hex").defaultValue = 'https://w.wallhaven.cc/full/lm/wallhaven-lm9oqy.jpg';
+    localStorage.setItem("imgchecksave", "1");	
+  }
+}
+
+function imgshow(){ 
+    var imgurl = 'url("' + document.getElementById("hex").value + '")';
+    localStorage.setItem("bgimgsave", document.getElementById('hex').value);
+    location.reload();
+} 
+
+function uicolorchck(){
+if (document.getElementById("checkbox2").checked == true){
+    $("#checkbox").prop("checked", false);
+    $("#checkbox3").prop("checked", false);
+    $('.apply').attr("onClick", "uichange()");
+    document.getElementById("hex").placeholder = "#000000";
+    document.getElementById("hex").defaultValue = '';
+    localStorage.setItem("uichecksave", "1");	
+  }
+}
+
+function uichange() {
+  var restrictedWords = new Array("black","rgb(0,0,0)","#000000","rgba(0,0,0,1)","rgba(0,0,0,1)", localStorage.getItem("bgcolorsave"));
   var txtInput = document.getElementById("hex").value;
   var error = 0;
   for (var i = 0; i < restrictedWords.length; i++) {
@@ -29,44 +104,11 @@ function colorpicker(){
       alert("White Theme can brake the websites look!")
   }
   else {
-    $("body").removeAttr("style");
-    $('body, .text1, .button1:active, li a:hover').css("background",$('#hex').val());
-    $(".box").toggleClass("box-show");
-    localStorage.setItem("bgcolorsave", document.getElementById('hex').value);
-    localStorage.setItem("imgchecksave", "0");
-}
-}
-function menubtn(){
-    Array.from(document.querySelectorAll("ul")).forEach(el => el.classList.toggle("show"));
-}
-function hidden() {
-  Array.from(document.querySelectorAll(".hidden")).forEach(el => el.classList.add("slowmoshow"));  
-  setTimeout(function(){Array.from(document.querySelectorAll(".hidden")).forEach(el => el.classList.remove("hidden")); },200);
-  setTimeout(function(){Array.from(document.querySelectorAll(".slowmoshow")).forEach(el => el.classList.remove("slowmoshow")); },1300);
-}
-
-function imgchck(){
-if (document.getElementById("checkbox").checked == true){
-    $('.apply').attr("onClick", "imgshow()");
-    document.getElementById("hex").placeholder = "https://somewebsite/image.png";
-    document.getElementById("hex").defaultValue = 'https://w.wallhaven.cc/full/lm/wallhaven-lm9oqy.jpg';
-    localStorage.setItem("imgchecksave", "1");	
-  } else {
-    $('.apply').attr("onClick", "colorpicker()");
-    document.getElementById("hex").placeholder = "#000000";
-    document.getElementById("hex").defaultValue = '';
+   localStorage.setItem("uicolorsave", document.getElementById('hex').value);
+   $(".box").toggleClass("box-show");
+   location.reload();
   }
 }
-
-function imgshow(){ 
-    var imgurl = 'url("' + document.getElementById("hex").value + '")';
-    localStorage.setItem("bgimgsave", document.getElementById('hex').value);
-    $("body").removeAttr("style");
-    $('body').css("background-image", imgurl);
-    $('.text1').css("background-image", "");$('.text1, .text2').addClass("pic");   
-    $('.container').css("width", "auto");
-    $(".box").toggleClass("box-show");
-} 
 
 var velocity = 0.2;
 function update(){ 
@@ -103,6 +145,7 @@ document.addEventListener("keyup", event => {
 });
 if (typeof(Storage) !== "undefined") {
   bgload();
+  uiload();
 } else {}
 function bgload() {
   if (localStorage.getItem("imgchecksave") == 1) {
@@ -112,10 +155,35 @@ function bgload() {
     $('.text1, .text2, .aboutme').addClass("pic");
     $('.container').css("width", "auto");
     }
-
-
    else {
    $('body, .text1, .button1:active, li a:hover').css("background", localStorage.getItem("bgcolorsave"));
-  }
-$(".box").removeClass("box-show");
+   }
 }
+function uiload() {
+ if (localStorage.getItem("uichecksave") == 1) {
+   sheet.insertRule(".uicolor {color:" + localStorage.getItem("uicolorsave") + "; }", 0);
+   sheet.insertRule(".uicolormenu:hover {background-color:" + localStorage.getItem("uicolorsave") + "; }", 0);
+   sheet.insertRule("ul.uicolor, .box {box-shadow: 0 0px 40px 0 " + localStorage.getItem("uicolorsave") + ",0 0px 100px 10px " + localStorage.getItem("uicolorsave") +";  }", 0);
+   sheet.insertRule("h2.uicolor, .apply.uicolorapply {background-color:" + localStorage.getItem("uicolorsave") + "; }" , 0);
+   sheet.insertRule(".button.uicolor {color:" + localStorage.getItem("uicolorsave") + "; }" , 0);
+   sheet.insertRule(".button.uicolor::before {background-color:" + localStorage.getItem("uicolorsave") + "; }" , 0);
+   sheet.insertRule(".button.uicolor:active {box-shadow: 0 0px 40px 0 " + localStorage.getItem("uicolorsave") + ",0 0px 100px 10px " + localStorage.getItem("uicolorsave") +";  }" , 0);
+   sheet.insertRule(".button.uicolor:hover {color: white;}" , 0);
+
+   $('.text2, .button1, .btn, .menubtn, ul, .box h2, .box, strong, .text1, .playlist').addClass("uicolor");
+   $('.box .apply').addClass("uicolorapply");
+   $('ul a').addClass("uicolormenu");
+   }
+ else {}
+}
+$('.restore').click(function(e) {
+    e.preventDefault();
+    if (window.confirm("This action will restore the page to it's original state. Are you sure?")) {
+    localStorage.setItem("imgchecksave", "0");
+    localStorage.setItem("uicolorsave", "0");
+    localStorage.setItem("bgcolorsave", "#000000");
+    localStorage.setItem("uichecksave", "0");
+    localStorage.setItem("bgcolorsave", "black")
+    location.reload();
+   }
+  });
