426f5e9c4cf26023e6e2aac3673538a309aa32eb
pxseu <57842793+pxseu@users.noreply.github.com>
2020-02-28 23:51:49 +0100

    Add files via upload

--- dragonware/index/js/main.js
+++ dragonware/index/js/main.js
@@ -1,13 +1,5 @@
 //yeah coding
 
-unhide();
-
-function scroll1() {
-  $("html, body").animate({ scrollTop: $('#software1').offset().top }, 1000);
-}
-
-function unhide() {
-  $(".hidden").addClass("slow_show");
-  setTimeout(function(){$(".hidden").removeClass("hidden"); },200);
-  setTimeout(function(){$(".slow_show").removeClass("slow_show"); },1300);
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
 }
