7669ae9952d659779e963cac255d5b9c3a737c51
pxseu <57842793+pxseu@users.noreply.github.com>
2020-02-29 17:02:09 +0100

    update

--- test/dragonware/essential/js/main.js
+++ test/dragonware/essential/js/main.js
@@ -2,3 +2,7 @@ function link1() { window.location.href = "index.html";}
 function link2() { window.location.href = "programy.html";}
 function link3() { window.location.href = "o_nas.html";}
 function link4() { window.location.href = "kontakt.html";}
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
