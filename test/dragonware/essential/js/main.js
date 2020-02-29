84c2d7e8429f94d94ffda1c9b594c870e622528c
pxseu <57842793+pxseu@users.noreply.github.com>
2020-02-29 12:52:38 +0100

    tests

---
+++ test/dragonware/essential/js/main.js
@@ -0,0 +1,4 @@
+function link1() { window.location.href = "index.html";}
+function link2() { window.location.href = "programy.html";}
+function link3() { window.location.href = "o_nas.html";}
+function link4() { window.location.href = "kontakt.html";}
---
+++ test/dragonware/index/js/main.js
@@ -0,0 +1,5 @@
+//yeah coding
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
