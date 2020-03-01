516f86a11235cc029c5492dd89ee851e59d8a105
pxseu <57842793+pxseu@users.noreply.github.com>
2020-03-01 22:37:27 +0100

    update

--- test/dragonware/programy/js/main.js
+++ test/dragonware/programy/js/main.js
@@ -2,6 +2,15 @@ function product1() { $( ".productimg1" ).toggleClass("productimgzoomed"); }
 function product2() { $( ".productimg2" ).toggleClass("productimgzoomed"); }
 function product3() { $( ".productimg3" ).toggleClass("productimgzoomed"); }
 function product4() { $( ".productimg4" ).toggleClass("productimgzoomed"); }
+function product5() { $( ".productimg5" ).toggleClass("productimgzoomed"); }
+function product6() { $( ".productimg6" ).toggleClass("productimgzoomed"); }
+
+function productid1() { alert("id = 9457372"); }
+function productid2() { alert("id = 2145632"); }
+function productid3() { alert("id = 2154666"); }
+function productid4() { alert("id = 5623521"); }
+function productid5() { alert("id = 2345567"); }
+function productid6() { alert("id = 8453514"); }
 
 
 
