5fa345c008c0b249ccc92285f4ead11ec9d4a9bb
pxseu <57842793+pxseu@users.noreply.github.com>
2020-03-07 23:09:38 +0100

    update

--- test/dragonware/index/js/main.js
+++ test/dragonware/index/js/main.js
@@ -1,1 +1,8 @@
 function moreaboutus() { window.location.href = "o_nas.html";}
+$( window ).resize(function() {widthchck();});
+$( window ).ready(function() {widthchck();});
+widthchck()
+function widthchck() {
+	if ($( window ).width() < 1400) {	$(".product").css({"width":"800px"});	}
+	else {	$(".product").removeAttr("style");	}
+}
