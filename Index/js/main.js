bd2b0d7df42b66dcdec285710842afc3986938ae
pxseu <57842793+pxseu@users.noreply.github.com>
2020-03-12 13:38:52 +0100

    update

--- Index/js/main.js
+++ Index/js/main.js
@@ -1,3 +1,3 @@
 function click1(){window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";}
 function click2(){window.location.href = "https://modman276.github.io/ZeroTwo/index.html";}
-function helpforothers(){window.open("tel:1-800-273-8255");}
+function helpforothers(){window.location.href = "tel:1-800-273-8255";}
