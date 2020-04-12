125fa6f447e9c04f33ba8e8dbc3d732af568edf2
Kuba Ellwart <gigantgigantowy@gmail.com>
2020-04-12 20:02:35 +0200

    update

--- About/js/script.js
+++ About/js/script.js
@@ -1,3 +1,3 @@
 function discord(){alert("My discord: modman#6944");}
 function email(){window.location.href = "mailto:ellwartkuba@gmail.com";}
-function twitter(){window.location.href = "https://twitter.com/poseuxck";}
+function twitter(){window.location.href = "https://twitter.com/pxseu";}
