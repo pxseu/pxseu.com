ab87a49fbca2b8b92ef9913995016a5ea024ed6e
pxseu <57842793+pxseu@users.noreply.github.com>
2020-04-12 21:14:09 +0200

    update

--- About/js/script.js
+++ About/js/script.js
@@ -1,3 +1,4 @@
 function discord(){alert("My discord: modman#6944");}
 function email(){window.location.href = "mailto:ellwartkuba@gmail.com";}
 function twitter(){window.location.href = "https://twitter.com/pxseu";}
+function steam(){window.location.href = "https://steamcommunity.com/id/pxseu/";}
