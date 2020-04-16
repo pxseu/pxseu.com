36a01ed16cdef0136c78e57216f5b75473cb8a91
pxseu <57842793+pxseu@users.noreply.github.com>
2020-04-16 15:00:38 +0200

    update

--- Kaboot/js/script.js
+++ Kaboot/js/script.js
@@ -1,15 +1,37 @@
 function search() {
-  var url = 'https://www.vektrix.cc/kahoot/start?pin=' + document.getElementById("soucetxt").value;
+  var userinput = document.getElementById("soucetxt").value
+  var url = 'https://www.vektrix.cc/kahoot/start?pin=' + userinput;
   if (document.getElementById("soucetxt").value < 1 )  {
     alert("No pin entered");
   }  
   else {
-    $.ajax({
-      type: "POST",
-      url: url
-    });
-  $(".mainbody").addClass("hidden");
-  $(".done").addClass("afterpost");
+    var para = document.createElement("P");
+    para.innerHTML = "-$ Your kahoot game code is: "+ userinput;
+    document.getElementById("terminal").appendChild(para);
+	setTimeout(function(){
+		var para = document.createElement("P");
+		para.innerHTML = ".";
+		document.getElementById("terminal").appendChild(para);
+		setTimeout(function(){para.innerHTML = "..";}, 1000);
+		setTimeout(function(){para.innerHTML = "...";}, 2000);
+		setTimeout(function(){para.innerHTML = "....";}, 3000);
+		setTimeout(function(){para.innerHTML = ".....";}, 4000);
+		setTimeout(function(){para.innerHTML = "......";}, 5000);
+	 }, 1000);
+	setTimeout(function(){
+		 var para = document.createElement("P");
+         para.innerHTML = "-$ Initializing bots";
+         document.getElementById("terminal").appendChild(para);
+		 $.ajax({
+			type: "POST",
+			url: url
+		});
+		setTimeout(function(){ 
+            var para = document.createElement("P");
+            para.innerHTML = "-$ Bots have been sent";
+            document.getElementById("terminal").appendChild(para);}, 1000);
+    }, 6000);
+	
   }
 }
 function modman276(){
@@ -35,3 +57,4 @@ setInputFilter(document.getElementById("soucetxt"), function(value) {
     });
   });
 }
+
