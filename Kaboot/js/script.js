0456ed12ce0fb9f712330e03c4460cdb4ffb1600
Kuba Ellwart <gigantgigantowy@gmail.com>
2020-04-16 12:49:12 +0200

    update

--- Kaboot/js/script.js
+++ Kaboot/js/script.js
@@ -15,3 +15,23 @@ function search() {
 function modman276(){
   window.open("https://pxseu.cc");
 }
+setInputFilter(document.getElementById("soucetxt"), function(value) {
+  return /^\d*\.?\d*$/.test(value);
+});
+
+ function setInputFilter(textbox, inputFilter) {
+  ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
+    textbox.addEventListener(event, function() {
+      if (inputFilter(this.value)) {
+        this.oldValue = this.value;
+        this.oldSelectionStart = this.selectionStart;
+        this.oldSelectionEnd = this.selectionEnd;
+      } else if (this.hasOwnProperty("oldValue")) {
+        this.value = this.oldValue;
+        this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+      } else {
+        this.value = "";
+      }
+    });
+  });
+}
