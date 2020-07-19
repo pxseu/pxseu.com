1e38130d44026b1c7d9068afdb450333d71d4856
pxseu <57842793+pxseu@users.noreply.github.com>
2020-07-19 15:01:38 +0200

    update

--- www/Index/js/main.js
+++ www/Index/js/main.js
@@ -1,3 +1,3 @@
-function click1(){window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";}
-function click2(){window.location.href = "https://modman276.github.io/ZeroTwo/index.html";}
+function click1(){window.location.href = "/about";}
+function click2(){window.location.href = "/apps";}
 function helpforothers(){window.location.href = "tel:1-800-273-8255";}
