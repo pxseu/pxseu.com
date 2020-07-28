e35f1988a5e91ddd36616a264986e8119a8fa23f
Kuba Ellwart <57842793+Modman276@users.noreply.github.com>
2020-07-28 14:24:57 +0200

    update

--- www/Index/js/main.js
+++
@@ -1,3 +0,0 @@
-function click1(){window.location.href = "/about";}
-function click2(){window.location.href = "/apps";}
-function helpforothers(){window.location.href = "tel:1-800-273-8255";}
--- www/test/dragonware/index/js/main.js
+++
@@ -1,8 +0,0 @@
-function moreaboutus() { window.location.href = "o_nas.html";}
-$( window ).resize(function() {widthchck();});
-$( window ).ready(function() {widthchck();});
-widthchck()
-function widthchck() {
-	if ($( window ).width() < 1400) {	$(".product").css({"width":"800px"});	}
-	else {	$(".product").removeAttr("style");	}
-}
