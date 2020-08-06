e528fec4b25c4ab086285d71b5b42badec98ad00
Kuba Ellwart <57842793+Modman276@users.noreply.github.com>
2020-08-06 14:47:52 +0200

    update

--- www/Index/js/main.js
+++
@@ -1,3 +0,0 @@
-function click1(){window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";}
-function click2(){window.location.href = "https://modman276.github.io/ZeroTwo/index.html";}
-function helpforothers(){window.location.href = "tel:1-800-273-8255";}
