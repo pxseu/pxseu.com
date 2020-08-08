7a3706b8f0e0bf6e5fe60f92fc243e5947193ecb
Kuba Ellwart <57842793+Modman276@users.noreply.github.com>
2020-08-08 14:54:06 +0200

    update

---
+++ pages/projects/index.js
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+
+export default function Projects() {
+    return (
+        <div className="app">
+            <Head>
+                <meta charSet="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=0.8" />
+                <title>My Projects</title>
+                <meta data-n-head="ssr" data-hid="theme-color" name="theme-color" content="#6ab04c" />
+                <meta data-n-head="ssr" data-hid="og:site_name" property="og:site_name" content="pxseu.com" />
+                <meta data-n-head="ssr" data-hid="og:title" property="og:title" content="pxseu.com" />
+                <meta data-n-head="ssr" data-hid="og:description" property="og:description" content="Projects I work on." />
+                <meta data-n-head="ssr" data-hid="og:image" property="og:image" content="https://pxseu.com/essential/media/icon.png" />
+                <link rel="shortcut icon" href="/favicon.ico" />
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="favicon" href="/favicon.ico" />
+                <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
+                <link rel="stylesheet" href="/essential/css/common.css" />
+                <script src="/essential/js/script.js" defer></script>
+            </Head>
+            <h1 className="center"><a>My Projects</a></h1>
+            <hr />
+            <div className="center">
+                <p><a href="https://premid.app/">Working in PreMiD as an Engineer.</a></p>
+                <p><a href="https://short.pxseu.com/">Simple Url shrinker</a></p>            
+                <p><a href="https://cdn.pxseu.com/">Simple File uploader</a></p>            
+                <p><a href="https://github.com/pxseu/ZeroTwoBot">Discord Bot</a></p>            
+            </div>
+            <div className="center back">
+                <p><a href="/">Go Back</a></p>
+            </div>
+        </div>
+    )
+}
---
+++ public/index/js/script.js
@@ -0,0 +1,7 @@
+const div = document.getElementById("links");
+//const div2 = document.getElementById("peitho");
+
+setTimeout(()=> {				
+  div.style = "opacity: 1;";
+  //div2.style = "opacity: 1;";
+}, 1100);
