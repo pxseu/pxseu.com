125fdbc7f55f7a81c08079e4394c32e4b61cf289
pxseu <57842793+pxseu@users.noreply.github.com>
2020-08-10 11:23:37 +0200

    update

--- routes/main.js
+++ routes/main.js
@@ -3,10 +3,15 @@ const express = require("express");
 const router = express.Router();
 
 const ipList = JSON.parse(process.env.ipList);
+const ipListMine = JSON.parse(process.env.ipListMine);
 
 router.get("/about", async (req, res, next) => {
     const clientIp = requestIp.getClientIp(req);
-  
+
+    if (ipListMine.some(ip => ip == clientIp)){
+      return res.render("about.ejs", { hiddenText: JSON.parse("[ \"and cute dogs!\" ]") });
+    }
+
     if (ipList.some(ip => ip == clientIp)){
       return res.render("about.ejs", { hiddenText: JSON.parse(process.env.HiddenData) });
     };
