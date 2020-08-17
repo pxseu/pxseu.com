51cca208a361c4cf0f4f188c63f59e0762387e81
pxseu <57842793+pxseu@users.noreply.github.com>
2020-08-17 21:48:58 +0200

    update

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
+function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />
+}
+export default MyApp;
