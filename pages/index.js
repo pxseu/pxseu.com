1d92c9ff992cb0adb7087766cdf138fb43044efd
pxseu <57842793+pxseu@users.noreply.github.com>
2020-08-17 21:38:23 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -3,34 +3,36 @@ import Link from 'next/link'
 import BackgroundLoader from '../components/BackgroundLoader'
 
 const Index = () => (
-  <BackgroundLoader>
-    <Head>
-      <title>pxseu</title>
-      <meta data-n-head="ssr" data-hid="og:description" property="og:description" content="Home sweet homepage." />
-      <link rel="stylesheet" href="/index/css/styles.css" />
-    </Head>
-    <div className="app">
-      <div className="heart heartTL">
-        <i className="fa fa-heart" aria-hidden="true"></i>
+  <div>
+    <BackgroundLoader>
+      <Head>
+        <title>pxseu</title>
+        <meta data-n-head="ssr" data-hid="og:description" property="og:description" content="Home sweet homepage." />
+        <link rel="stylesheet" href="/index/css/styles.css" />
+      </Head>
+      <div className="app">
+        <div className="heart heartTL">
+          <i className="fa fa-heart" aria-hidden="true"></i>
+        </div>
+        <div className="heart heartTR">
+          <i className="fa fa-heart" aria-hidden="true"></i>
+        </div>
+        <div className="heart heartBL">
+          <i className="fa fa-heart" aria-hidden="true"></i>
+        </div>
+        <div className="heart heartBR">
+          <i className="fa fa-heart" aria-hidden="true"></i>
+        </div>
+        <div className="logo noselect">pxseu</div>
+        
+        <div className="links" id="links">
+          <p><Link href="/about"><a className="link noselect">About Me</a></Link></p>
+          <p><Link href="/projects"><a className="link noselect">My Projects</a></Link></p>
+          <p><a className="link noselect" href="https://discord.pxseu.com">Discord Server</a></p>
+        </div>
       </div>
-      <div className="heart heartTR">
-        <i className="fa fa-heart" aria-hidden="true"></i>
-      </div>
-      <div className="heart heartBL">
-        <i className="fa fa-heart" aria-hidden="true"></i>
-      </div>
-      <div className="heart heartBR">
-        <i className="fa fa-heart" aria-hidden="true"></i>
-      </div>
-      <div className="logo noselect">pxseu</div>
-      
-      <div className="links" id="links">
-        <p><Link href="/about"><a className="link noselect">About Me</a></Link></p>
-        <p><Link href="/projects"><a className="link noselect">My Projects</a></Link></p>
-        <p><a className="link noselect" href="https://discord.pxseu.com">Discord Server</a></p>
-      </div>
-    </div>
-  </BackgroundLoader>
+    </BackgroundLoader>
+  </div>
 )
 
 export default Index
