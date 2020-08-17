80c8007dbb4aebdf5270de611e48f80617389dd7
pxseu <57842793+pxseu@users.noreply.github.com>
2020-08-18 00:00:04 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const Index = () => (
       <Head>
         <title>pxseu</title>
         <meta data-n-head="ssr" data-hid="og:description" property="og:description" content="Home sweet homepage." />
-        <link rel="stylesheet" href="/index/css/styles.css" />
+        {/* <link rel="stylesheet" href="/index/css/styles.css" /> */}
       </Head>
       <div className="app">
         <div className="heart heartTL">
@@ -32,6 +32,167 @@ const Index = () => (
         </div>
       </div>
     </BackgroundLoader>
+    <style jsx>{`
+      .logo {
+        position: absolute;
+        left: 50%;
+        transform: translate(-50%);
+        top: 220px;
+        z-index: 2;
+        animation: 1.5s ease-out 0s 1 fadeIn;
+        cursor: pointer;
+        text-align: center;
+        color: #FFA9FF;
+        font-size: 150px;
+        font-family: "Gang of Three";
+      }
+      
+      .links {
+          position: absolute;
+          left: 50%;
+          transform: translate(-50%);
+          top: 400px;
+          z-index: 2;
+          animation: 1.5s ease-out 0s 1 fadeIn;
+          cursor: default;
+          text-align: center;
+      }
+      
+      p {
+          display: inline;
+          padding: 14px;
+          cursor: default;
+          text-align: center;
+      } 
+      
+      .link {
+          color: white;
+          font-family: 'Roboto', sans-serif;
+          text-decoration: none;
+          font-size: 30px;
+          transition: color 0.1s;
+          cursor: pointer;
+          position: relative;
+          top: 0;
+          transition: top ease 0.2s, color 0.1s;
+          
+          display: inline-block;
+          padding: 15px 20px;
+          position: relative;
+      }
+      
+      .link:after {    
+          background: none repeat scroll 0 0 transparent;
+          bottom: 0;
+          content: "";
+          display: block;
+          height: 2px;
+          left: 50%;
+          position: absolute;
+          background: #fff;
+          transition: width 0.3s ease 0s, left 0.3s ease 0s;
+          width: 0;
+      }
+      .link:hover:after { 
+          width: 100%; 
+          left: 0; 
+      }
+      
+      .link:hover {
+          color: #FFA9FF;
+          top: -2px;
+      }
+      
+      .heart {
+          position: absolute;
+          color: #FFA9FF;
+          display: none;
+          padding: 8px;
+      }
+      
+      .heartTL {
+          left: 0px;
+          top: 0px;
+      }
+      
+      .heartTR {
+          right: 0px;
+          top: 0px;
+      }
+      
+      .heartBL {
+          left: 0px;
+          bottom: 0px;
+      }
+      
+      .heartBR {
+          right: 0px;
+          bottom: 0px;
+      }
+      
+      .peitho {
+          position: relative;
+          top: 170px;
+          text-align: center;
+          animation: 1.5s ease-out 1s 1 fadeIn;
+          font-family: 'Roboto', sans-serif;
+          color: white;
+          opacity: 0;
+      }
+      
+      .noselect {
+          -webkit-touch-callout: none;
+          -webkit-user-select: none;
+          -khtml-user-select: none;
+          -moz-user-select: none;
+          -ms-user-select: none;
+          user-select: none;
+      }
+      
+      @media only screen and (max-width: 740px) {
+          .links p {
+            display: list-item;
+            padding: 0;
+          }
+        }
+      
+      @media only screen and (min-width: 1500px){
+          .heart, .heartR {
+              display: inline;
+          }
+      }
+      
+      @keyframes fadeIn {
+          0% {
+            opacity: 0;
+          }
+          100% {
+            opacity: 1;
+          }
+      }
+      
+      @keyframes in {
+          from {
+              transform: rotate(0deg);
+          }
+          to {
+              transform: rotate(360deg);
+          }
+      }
+      @keyframes out {
+          from {
+              transform: rotate(360deg);
+          }
+          to {
+              transform: rotate(0deg);
+          }
+      }
+      
+      @font-face {
+          font-family: "Gang of Three";
+          src: url("/index/font/go3v2.ttf");
+      }
+    `}</style>
   </div>
 )
 
