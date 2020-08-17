448043936f55fa7e46d7cd63c23e5af35c1440ff
pxseu <57842793+pxseu@users.noreply.github.com>
2020-08-17 20:58:49 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,34 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import BackgroundLoader from '../components/BackgroundLoader'
 
-export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>pxseu</title>
-        <meta charSet="UTF-8" />
-        <meta name="description" content="Pxseu's website!" />
-        <meta name="keywords" content="pxseu, poseuxck" />
-        <meta name="author" content="pxseu" />
-        <meta data-n-head="ssr" data-hid="theme-color" name="theme-color" content="#6ab04c" />
-        <meta data-n-head="ssr" data-hid="og:site_name" property="og:site_name" content="pxseu.com" />
-        <meta data-n-head="ssr" data-hid="og:title" property="og:title" content="pxseu.com" />
-        <meta data-n-head="ssr" data-hid="og:description" property="og:description" content="Home sweet homepage." />
-        <meta data-n-head="ssr" data-hid="og:image" property="og:image" content="https://www.pxseu.com/essential/media/icon.png" />
-        <meta name="viewport" content="width=device-width, initial-scale=0.8, maximum-scale=0.8, minimum-scale=0.8, user-scalable=no, minimal-ui" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-		    <link rel="icon" href="/favicon.ico" />
-        <link rel="favicon" href="/favicon.ico" />
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="/index/css/styles.css" />
-        <script src="/essential/js/particles.min.js" defer></script>
-        <script src="/essential/js/script.js" defer></script>
-        <script src="/index/js/script.js" defer></script>
-      </Head>
-      
-      <div className="app">
-        <div className="heart heartTL">
-          <i className="fa fa-heart" aria-hidden="true"></i>
-        </div>
-        <div className="heart heartTR">
-          <i className="fa fa-heart" aria-hidden="true"></i>
-        </div>
-        <div className="heart heartBL">
-          <i className="fa fa-heart" aria-hidden="true"></i>
-        </div>
-        <div className="heart heartBR">
-          <i className="fa fa-heart" aria-hidden="true"></i>
-        </div>
-        <div className="logo noselect">pxseu</div>
-        
-        <div className="links" id="links">
-          <p><a className="link noselect" href="/about">About Me</a></p>
-          <p><a className="link noselect" href="/projects">My Projects</a></p>
-          <p><a className="link noselect" href="https://discord.pxseu.com">Discord Server</a></p>
-        </div>
-      </div>
-      <div id="particles-js" className="particles-js"></div>
+const Index = () => (
+  <BackgroundLoader>
+    <Head>
+      <title>pxseu</title>
+      <meta data-n-head="ssr" data-hid="og:description" property="og:description" content="Home sweet homepage." />
+      <link rel="stylesheet" href="/index/css/styles.css" />
+    </Head>
+    <div className="heart heartTL">
+      <i className="fa fa-heart" aria-hidden="true"></i>
     </div>
-  )
-}
+    <div className="heart heartTR">
+      <i className="fa fa-heart" aria-hidden="true"></i>
+    </div>
+    <div className="heart heartBL">
+      <i className="fa fa-heart" aria-hidden="true"></i>
+    </div>
+    <div className="heart heartBR">
+      <i className="fa fa-heart" aria-hidden="true"></i>
+    </div>
+    <div className="logo noselect">pxseu</div>
+    
+    <div className="links" id="links">
+      <p><Link href="/about"><a className="link noselect">About Me</a></Link></p>
+      <p><Link href="/projects"><a className="link noselect">My Projects</a></Link></p>
+      <p><a className="link noselect" href="https://discord.pxseu.com">Discord Server</a></p>
+    </div>
+  </BackgroundLoader>
+)
+
+export default Index
