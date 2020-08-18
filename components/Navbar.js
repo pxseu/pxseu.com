382b656257c874658e2f532e43ccd47383c20890
pxseu <57842793+pxseu@users.noreply.github.com>
2020-08-18 17:11:38 +0200

    update

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,14 +6,12 @@ const Navbar = () => {
     return (
         <header className={ navbarClasses.join(" ")}>
             <Link href="/"><p className="logoNavbar">pxseu</p></Link>
-
             <nav className="navigation">
-                <Link href="/"><a>Home</a></Link>
                 <Link href="/about"><a>About</a></Link>
                 <Link href="/projects"><a>Projects</a></Link>
             </nav>
-
         </header>
     )
 };
+
 export default Navbar;
