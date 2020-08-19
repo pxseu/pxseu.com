03df79569f0cafaedee45183b32c456f64ba7d36
pxseu <57842793+pxseu@users.noreply.github.com>
2020-08-19 20:45:27 +0200

    update

--- pages/love/index.js
+++ pages/love/index.js
@@ -7,9 +7,7 @@ const LovesIndex = () => (
             <title>I love you &lt;333</title>
             <meta data-n-head="ssr" data-hid="og:description" property="og:description" content="I love you cutie <333" />
         </Head>
-        <hr />
         <h1 className="center noselect"><a href="//cdn.pxseu.com/meAndHer.jpg">I love you cutie &lt;333</a></h1>
-        <hr />
     </DefaultLayout>
 )
 
