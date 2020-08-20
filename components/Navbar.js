eb76b9de6fe93700560ddf355f75dafe2cd5b80e
Kuba Ellwart <57842793+Modman276@users.noreply.github.com>
2020-08-20 20:31:54 +0200

    update

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,52 @@
 import Link from 'next/link'
+import { useState, useEffect } from 'react'
 
 const Navbar = () => {
-     const navbarClasses=[ 'navbar' ];
+    const navbarClasses=[ 'navbar' ];
+
+    const [loaded, setLoaded] = useState(false);
+
+    const [navToggle, setNavToggle] = useState(false);
+
+    const [smallScreen, setsmallScreen] = useState(false);
+
+    const resizeHandler = () => {
+        setNavToggle(false);
+        if (window.innerWidth < 600) setsmallScreen(true);
+        else setsmallScreen(false);
+    }
+
+    useEffect(() => {
+        if (!loaded){
+            resizeHandler();
+            setLoaded(true);
+        }
+        window.addEventListener("resize", resizeHandler);
+        return () => {
+          window.removeEventListener("resize", resizeHandler);
+        };
+    });
 
     return (
-        <header className={ navbarClasses.join(" ")}>
-            <Link href="/"><p className="logoNavbar">pxseu</p></Link>
-            <nav className="navigation">
-                <Link href="/about"><a>About</a></Link>
-                <Link href="/projects"><a>Projects</a></Link>
-            </nav>
-        </header>
+        <>
+            <header className={ navbarClasses.join(" ")}>
+                <Link href="/"><p className="logoNavbar">pxseu</p></Link>
+                <nav className={ smallScreen ? "hidden" : "navigation"}>
+                    <Link href="/about"><a>About</a></Link>
+                    <Link href="/projects"><a>Projects</a></Link>
+                </nav>
+                <p className={ smallScreen ? "navigation navIcon" : "hidden"} onClick={ () => setNavToggle(true) }>
+                    <i className="fas fa-bars"></i>
+                </p>
+            </header>
+            <div className={navToggle ? "navOverlay show" : "navOverlay"}>
+                <a className="closebtn" onClick={ () => setNavToggle(false) }>&times;</a>
+                <div className="navOverlay-content">
+                    <Link href="/about"><a>About</a></Link>
+                    <Link href="/projects"><a>Projects</a></Link>
+                </div>
+            </div>
+        </>
     )
 };
 
--- public/essential/js/darling.js
+++ public/essential/js/darling.js
@@ -1,6 +1,4 @@
-//copyright@pxseu.cc  all rights reserved
-
-var darlingKeys = {
+const DarlingKeys = {
   68: 'd',
   65: 'a',
   82: 'r',
@@ -9,18 +7,16 @@ var darlingKeys = {
   78: 'n',
   71: 'g'
 };
-var darlingCode = ['d', 'a', 'r', 'l', 'i', 'n', 'g'];
-var darlingCodePosition = 0;
+const DarlingCode = ['d', 'a', 'r', 'l', 'i', 'n', 'g'];
+let darlingCodePosition = 0;
 
 document.addEventListener('keydown', function(e) {
-  var key = darlingKeys[e.keyCode];
-  var requiredKey = darlingCode[darlingCodePosition];
+  const key = DarlingKeys[e.keyCode];
+  const requiredKey = DarlingCode[darlingCodePosition];
 
   if (key == requiredKey) {
-
     darlingCodePosition++;
-
-    if (darlingCodePosition == darlingCode.length) {
+    if (darlingCodePosition == DarlingCode.length) {
       darlingCodePassed();
       darlingCodePosition = 0;
     }
@@ -30,62 +26,14 @@ document.addEventListener('keydown', function(e) {
 });
 
 function darlingCodePassed() {
-  if ($("body").hasClass("monika")){
-  	alert("Do not even try!");
-  }
-  else {
-  	var audio2 = new Audio('../Essential/media/darling.mp3');
-  	audio2.play();
-  	audio2.onended=function(){
-		$(".uicolor, .uicolormenu").removeClass("uicolor","uicolormenu");
-  		alert("Darling I found you!");
- 		pinker1();
-  		$('#clickmebtn').html("Darling!");
- 	}
+  const DarlingCodePassedAudio = new Audio('../Essential/media/darling.mp3');
+  DarlingCodePassedAudio.play();
+  DarlingCodePassedAudio.onended=function(){
+  $(".uicolor, .uicolormenu").removeClass("uicolor","uicolormenu");
+    alert("Darling I found you!");
+  pinker1();
+    $('#clickmebtn').html("Darling!");
  }
 }
 
-
-var audioditfx = new Audio('../Essential/media/ditfx.mp3');
-
-function ditfxpause(){
-    audioditfx.pause();
-    document.getElementById( "clickmebtn" ).setAttribute( "onClick", "ditfxunpause()" );
-}
-function ditfxunpause(){
-    audioditfx.volume = 0.1;
-    audioditfx.play();
-    document.getElementById( "clickmebtn" ).setAttribute( "onClick", "ditfxpause()" );
-}
-function pinker1(){
-  $('body').css("background-image", "");
-  if ($(window).width() < 800) {
-    Array.from(document.querySelectorAll("body")).forEach(el => el.classList.add("darlingm"));
-    Array.from(document.querySelectorAll("body")).forEach(el => el.classList.remove("darling"));
-  }
-  else { 
-    Array.from(document.querySelectorAll("body")).forEach(el => el.classList.add("darling"));
-    Array.from(document.querySelectorAll("body")).forEach(el => el.classList.remove("darlingm"));
-  }
-    Array.from(document.querySelectorAll("body")).forEach(el => el.classList.remove("konami","monika"));
-    Array.from(document.querySelectorAll(".text1")).forEach(el => el.classList.add("transparent"));
-    Array.from(document.querySelectorAll(".text2, .contactStuff, h1 strong, button, .btn, .menubtn, li a, .box h2, .apply, .clock, .box, .playlist, ul")).forEach(el => el.classList.add("pink"));
-    $('#clickmebtn').attr("onClick", "ditfxunpause()" );
-    $('#gifbox').remove();
-    $(".text1").css("background", "rgba(0, 0, 0, 0.4)");
-    if (($('#clickmebtn').length)&&($(window).width() > 800)) {
-          var zrtsugifmake = document.createElement("IMG");
-          zrtsugifmake.id = "gifbox";
-          zrtsugifmake.src='../Essential/media/zrtsu_static.gif';
-          zrtsugifmake.setAttribute( "onClick", "zrtsu_change()" );
-          zrtsugifmake.classList.add("gifbox");
-          document.body.appendChild(zrtsugifmake);
-  
- } 
-}
-function zrtsu_change(){
-  var zrtsugifbox = document.getElementById('gifbox');
-  zrtsugifbox.src='../Essential/media/zrtsu.gif';
-  zrtsugifbox.setAttribute( "onClick", "" );
-  zrtsugifbox.classList.add("pinkshadow");
-}
+const audioditfx = new Audio('../Essential/media/ditfx.mp3');
