cd7e23789f28530a31eed8d5c3cfc91991eb2cc2
Kuba Ellwart <57842793+Modman276@users.noreply.github.com>
2020-08-20 22:00:25 +0200

    update

---
+++ pages/contact/index.jsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import DefaultLayout from '../../components/DefaultLayout'
+
+const Contact = () => (
+    <DefaultLayout>
+        <Head>
+            <title>Contact / Socials</title>
+            <meta data-n-head="ssr" data-hid="og:description" property="og:description" content="Contact / Socials" />
+        </Head>
+        <h1 className="center noselect"><a>Contact / Socials</a></h1>
+        <hr />
+        <div className="center noselect">
+            <p><a href="https://github.com/pxseu">Github</a></p>
+            <p><a href="https://twitter.com/pxseu">Twitter</a></p>            
+            <p><a href="mailto:me@pxseu.com">Email</a></p>            
+        </div>  
+    </DefaultLayout>    
+)
+
+export default Contact
