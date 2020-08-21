2f9fec872e1899ec17c392cb5056854f1ac9199c
Kuba Ellwart <57842793+Modman276@users.noreply.github.com>
2020-08-22 01:36:42 +0200

    legal stuff lol

---
+++ pages/legal-stuff/index.jsx
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+import DefaultLayout from '../../components/DefaultLayout'
+
+const LovesIndex = () => (
+    <>
+        <DefaultLayout>
+            <Head>
+                <title>Terms of Service | Privacy Policy</title>
+                <meta data-n-head="ssr" data-hid="og:description" property="og:description" content="Terms of Service" />
+            </Head>
+            <h1 className="center noselect"><a>Terms of Service and Privacy Policy.</a></h1>
+            <hr />
+            <div className="center noselect">
+                <p>1. Any form of spamming will not be tolerated.</p>
+                <p>2. Adult contenet (gore, porn, deaths, naked photos) will not be tolerated.</p>
+                <p>3. I do not guarantee the state of the files for you. They can be delte at any point for storage / update reasons.</p>
+                <p>4. Only one account is allowed per one user. (no second accounts will be tolerated)</p>
+                <p>5. Bans will be given without a reason and they might (not) be permanent.</p>
+            </div>
+            <hr />
+            <h1 className="center noselect"><a>Privacy Policy</a></h1>
+            <div className="center noselect">
+                <p>I value your privacy and your trust.</p>
+                <p>All of the data that you input / give will be stored in the propper way.</p>
+                <p>All your data can be delete with the account deleteion process.</p>
+            </div>
+            <hr />
+            <h4 className="center noselect"><a>If you have any question join my discord server!</a></h4>
+        </DefaultLayout>
+    </>
+)
+
+
+export default LovesIndex
