835e9527685b76ced5cff2fb99256c903f6c41d0
Kuba Ellwart <57842793+Modman276@users.noreply.github.com>
2020-08-21 12:09:58 +0200

    update

--- pages/love/index.jsx
+++ pages/love/index.jsx
@@ -1,14 +1,31 @@
 import Head from 'next/head'
+import { useState } from 'react'
 import DefaultLayout from '../../components/DefaultLayout'
 
-const LovesIndex = () => (
-    <DefaultLayout>
-        <Head>
-            <title>I love you &lt;333</title>
-            <meta data-n-head="ssr" data-hid="og:description" property="og:description" content="I love you cutie <333" />
-        </Head>
-        <h1 className="center noselect"><a href="//cdn.pxseu.com/meAndHer.jpg">I love you cutie &lt;333</a></h1>
-    </DefaultLayout>
-)
+const LovesIndex = () => {
+    const [loveClick, setLoveClick] = useState(false);
+
+    return (
+        <>
+            <DefaultLayout>
+                <Head>
+                    <title>I love you &lt;333</title>
+                    <meta data-n-head="ssr" data-hid="og:description" property="og:description" content="I love you cutie <333" />
+                </Head>
+                <h1 className="center noselect">
+                    <a onClick={ ()=> setLoveClick(true) }>I love you cutie &lt;333</a>
+                </h1>
+                <div className={ loveClick ? "imageDiv" : "hidden" }>
+                    <img src="https://cdn.pxseu.com/meAndHer.jpg" />
+                </div>
+            </DefaultLayout>
+            <style jsx>{`
+                .imageDiv {
+                    overflow: hidden;
+                }
+            `}</style>
+        </>
+    )
+}
 
 export default LovesIndex
