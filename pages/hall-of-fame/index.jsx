4685b79393f1a1f0f15078dab122bc53dee4f191
Kuba Ellwart <57842793+Modman276@users.noreply.github.com>
2020-08-27 20:01:32 +0200

    update

--- pages/hall-of-fame/index.jsx
+++ pages/hall-of-fame/index.jsx
@@ -1,19 +1,26 @@
-import Head from 'next/head'
-import DefaultLayout from '../../components/DefaultLayout'
+import Head from 'next/head';
+import DefaultLayout from '../../components/DefaultLayout';
 
 const HallOfFame = () => (
-    <DefaultLayout>
-        <Head>
-            <title>Hall of fame</title>
-            <meta data-n-head="ssr" data-hid="og:description" property="og:description" content="Hall of fame" />
-        </Head>
-        <h1 className="center noselect"><a>These people helped with the creation of the website</a></h1>
-        <hr />
-        <div className="center noselect">
-            <p>Peitho | spell checking and langauge corrections</p>
-            <p>Vek | told me about security.txt</p>
-        </div>  
-    </DefaultLayout>    
-)
+	<DefaultLayout>
+		<Head>
+			<title>Hall of fame</title>
+			<meta
+				data-n-head='ssr'
+				data-hid='og:description'
+				property='og:description'
+				content='Hall of fame'
+			/>
+		</Head>
+		<h1 className='center noselect'>
+			<a>These people helped with the creation of the website</a>
+		</h1>
+		<hr />
+		<div className='center noselect'>
+			<p>Peitho | spell checking and langauge corrections</p>
+			<p>Vek | told me about security.txt</p>
+		</div>
+	</DefaultLayout>
+);
 
-export default HallOfFame
+export default HallOfFame;
