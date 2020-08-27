673085d5c8719371395b154c4f2177e5c62c4d3c
pxseu <57842793+pxseu@users.noreply.github.com>
2020-08-27 19:58:58 +0200

    update

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -1,30 +1,55 @@
-import Head from 'next/head'
-import DefaultLayout from '../../components/DefaultLayout'
+import Head from 'next/head';
+import DefaultLayout from '../../components/DefaultLayout';
+import Modal from '../../components/Modal';
+import { useState } from 'react';
 
-const About = () => (
-    <DefaultLayout>
-        <Head>
-            <title>About Me</title>
-            <meta data-n-head="ssr" data-hid="og:description" property="og:description" content="About me." />
-        </Head>
-    
-        <h1 className="center noselect"><a>About Me</a></h1>
-        <hr />
-        <div className="center noselect">
-            <p>Web and Back End developer.</p>
-            <p>Javascript lover.</p>
-            <p>Always up for a new challange.</p>
-            <p>16 years old.</p>
-            <p>I like anime and manga (and color pink uwu).</p>
-            <p>I love my cute and amazing girlfriend. (<a href="https://julie420.github.io">Peitho</a>)</p>
-            <p>I use these languages:</p>
-            <p>C++, C#, TS, JS, Python</p>
-            <p>I have experience with:</p>
-            <p>Next.js, React, Express, MongoDB, Discord.js, EJS, Passport, Browser Extensions, Cross Platform Apps</p>
-            <p>Currently learning / getting better with:</p>
-            <p>Reacts and Next.js with Express</p>
-        </div> 
-    </DefaultLayout>    
-)
+const About = () => {
+	const [isOpen, setIsOpen] = useState(false);
 
-export default About
+	return (
+		<DefaultLayout>
+			<Head>
+				<title>About Me</title>
+				<meta
+					data-n-head='ssr'
+					data-hid='og:description'
+					property='og:description'
+					content='About me.'
+				/>
+			</Head>
+
+			<h1 className='center noselect'>
+				<a>About Me</a>
+			</h1>
+			<hr />
+			<div className='center noselect'>
+				<p>Web and Back End developer.</p>
+				<p>Javascript lover.</p>
+				<p>Always up for a new challange.</p>
+				<p>16 years old.</p>
+				<p>I like anime and manga (and color pink uwu).</p>
+				<p>
+					I love my cute and amazing girlfriend. (
+					<a onClick={() => setIsOpen(true)}>Peitho</a>)
+				</p>
+				<p>I use these languages:</p>
+				<p>C++, C#, TS, JS, Python</p>
+				<p>I have experience with:</p>
+				<p>
+					Next.js, React, Express, MongoDB, Discord.js, EJS, Passport, Browser
+					Extensions, Cross Platform Apps
+				</p>
+				<p>Currently learning / getting better with:</p>
+				<p>Reacts and Next.js with Express</p>
+			</div>
+			<Modal open={isOpen} onClose={() => setIsOpen(false)}>
+				<p>
+					The website is <a href='https://julie420.github.io'>here.</a>
+				</p>
+				<p className='warn'>Warning contains an ip logger.</p>
+			</Modal>
+		</DefaultLayout>
+	);
+};
+
+export default About;
