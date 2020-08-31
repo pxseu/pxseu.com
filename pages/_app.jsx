47699f20fc9dabf8586bc28d63f0011ae778657c
pxseu <ellwartkuba@gmail.com>
2020-08-31 21:25:07 +0200

    mobile support XDDDDDDDDD

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,11 @@
-import Router from 'next/router';
-import NProgress from 'nprogress';
-import 'nprogress/nprogress.css';
-import '../styles/global.css';
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import "../styles/global.css";
 
-Router.events.on('routeChangeStart', () => NProgress.start());
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 const App = ({ Component, pageProps }) => (
 	<>
