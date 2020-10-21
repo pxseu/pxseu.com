7a689ec30d1e000a9807c8ab6c4a6067a0e02a94
pxseu <57842793+pxseu@users.noreply.github.com>
2020-10-21 12:46:56 +0200

    🔄recoded some stuff

--- api.js
+++ api.js
@@ -2,6 +2,19 @@ const router = require("express").Router();
 const webhook = require("webhook-discord");
 const rateLimit = require("express-rate-limit");
 
+const methodCheck = {
+	post: (req, res, next) => {
+		const method = req.method;
+
+		if (method != "POST") {
+			return res.json({
+				error: "Method not allowed!",
+			});
+		}
+		next();
+	},
+};
+
 const sendMessageLimiter = rateLimit({
 	windowMs: 10 * 60 * 1000, // 10 minutes
 	max: 1,
@@ -12,37 +25,46 @@ const sendMessageLimiter = rateLimit({
 
 router.use(require("express").json());
 
-router.post("/v1/sendMessage", sendMessageLimiter, async (req, res) => {
-	const body = await req.body;
+router.use(
+	"/v1/sendMessage",
+	methodCheck.post,
+	async (req, res, next) => {
+		const body = await req.body;
 
-	if (body.content == "" || body.content.trim() == "") {
-		return res.status(400).json({
-			error: "Cannot send emtpy message!",
+		if (body.content == undefined || body.content.trim() == "") {
+			return res.status(400).json({
+				error: "Cannot send emtpy message!",
+			});
+		}
+
+		next();
+	},
+	sendMessageLimiter,
+	async (req, res) => {
+		const body = await req.body;
+		const Hook = new webhook.Webhook(process.env.WEBHOOK);
+		const embed = new webhook.MessageBuilder();
+
+		embed.setName("pxseu messenger");
+		embed.setAvatar(
+			"https://cdn.discordapp.com/avatars/645330135527981069/3440c4def2a42777de2ccafba45adf02.webp?size=512"
+		);
+		embed.setAuthor(
+			"Anonymous",
+			"https://cdn.discordapp.com/avatars/645330135527981069/3440c4def2a42777de2ccafba45adf02.webp?size=512",
+			"https://www.pxseu.com/other/message"
+		);
+		embed.setURL("https://www.pxseu.com/other/message");
+		embed.setTitle("New Message!");
+		embed.setDescription(`Content: \n${body.content.trim()}`);
+		embed.setColor("#3399ff");
+		embed.setTime();
+		await Hook.send(embed);
+
+		res.json({
+			content: body.content.trim(),
 		});
 	}
-
-	const Hook = new webhook.Webhook(process.env.WEBHOOK);
-
-	const embed = new webhook.MessageBuilder();
-	embed.setName("pxseu messenger");
-	embed.setAvatar(
-		"https://cdn.discordapp.com/avatars/645330135527981069/3440c4def2a42777de2ccafba45adf02.webp?size=512"
-	);
-	embed.setAuthor(
-		"pxseu",
-		"https://cdn.discordapp.com/avatars/645330135527981069/3440c4def2a42777de2ccafba45adf02.webp?size=512",
-		"https://www.pxseu.com/other/messages"
-	);
-	embed.setURL("https://pxseu.cc");
-	embed.setTitle("New Message!");
-	embed.setDescription(`Content: \n\n${body.content.trim()}`);
-	embed.setColor("#3399ff");
-	embed.setTime();
-	await Hook.send(embed);
-
-	res.json({
-		content: body.content.trim(),
-	});
-});
+);
 
 module.exports = router;
