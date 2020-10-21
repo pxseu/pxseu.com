408a465e201c9724c8daa78895d837486e80eb6e
pxseu <gigantgigantowy@gmail.com>
2020-10-21 11:49:34 +0200

    🐛 wrong url in embeds

--- api.js
+++ api.js
@@ -31,11 +31,11 @@ router.post("/v1/sendMessage", sendMessageLimiter, async (req, res) => {
 	embed.setAuthor(
 		"pxseu",
 		"https://cdn.discordapp.com/avatars/645330135527981069/3440c4def2a42777de2ccafba45adf02.webp?size=512",
-		"https://pxseu.cc"
+		"https://www.pxseu.com/other/messages"
 	);
 	embed.setURL("https://pxseu.cc");
 	embed.setTitle("New Message!");
-	embed.setDescription(`Content: \n${body.content.trim()}`);
+	embed.setDescription(`Content: \n\n${body.content.trim()}`);
 	embed.setColor("#3399ff");
 	embed.setTime();
 	await Hook.send(embed);
