62e2abac2a73dc2d81b8058e764ae362a5bb84c7
pxseu <57842793+pxseu@users.noreply.github.com>
2020-10-23 00:10:56 +0200

    💄fixed weird text in "/other"

--- pages/other/index.jsx
+++ pages/other/index.jsx
@@ -3,43 +3,50 @@ import Link from "next/link";
 import DefaultLayout from "../../components/DefaultLayout";
 
 const OtherIndex = () => (
-	<DefaultLayout>
+	<>
 		<Head>
-			<title>Grab a waifu!</title>
+			<title>Other stuff.</title>
 			<meta
 				data-n-head='ssr'
 				data-hid='og:description'
 				property='og:description'
-				content='Grab a waifu!'
+				content='Other stuff.'
 			/>
 		</Head>
-		<h1 className='center noselect'>
-			<a>Other stuff</a>
-		</h1>
-		<hr />
-		<div className='center noselect'>
-			<Link href='/other/yiff'>
-				<h3>
-					<a>Yiff</a>
-				</h3>
-			</Link>
-			<Link href='/other/waifu'>
-				<h3>
-					<a>Waifu</a>
-				</h3>
-			</Link>
-			<Link href='/other/message'>
-				<h3>
-					<a>Message me!</a>
-				</h3>
-			</Link>
-			<Link href='/hall-of-fame'>
-				<h3>
-					<a>Hall Of Fame</a>
-				</h3>
-			</Link>
-		</div>
-	</DefaultLayout>
+		<DefaultLayout>
+			<h1 className='center noselect'>
+				<a>Other stuff</a>
+			</h1>
+			<hr />
+			<div className='mainButtons center noselect'>
+				<Link href='/other/yiff'>
+					<p>
+						<a>Yiff</a>
+					</p>
+				</Link>
+				<Link href='/other/waifu'>
+					<p>
+						<a>Waifu</a>
+					</p>
+				</Link>
+				<Link href='/other/message'>
+					<p>
+						<a>Message me!</a>
+					</p>
+				</Link>
+				<Link href='/hall-of-fame'>
+					<p>
+						<a>Hall Of Fame</a>
+					</p>
+				</Link>
+			</div>
+		</DefaultLayout>
+		<style jsx>{`
+			.mainButtons p {
+				font-size: 25px;
+			}
+		`}</style>
+	</>
 );
 
 export default OtherIndex;
