ccea64041cfc94e6a43958ed14349a27e1a7f9d1
pxseu <57842793+pxseu@users.noreply.github.com>
2020-11-24 10:33:18 +0100

    better scam switch

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -3,16 +3,24 @@ import DefaultLayout from "../../components/DefaultLayout";
 const Contact = () => (
 	<DefaultLayout title={"Contact / Socials"}>
 		<p>
-			<a href='https://github.com/pxseu'>Github</a>
+			<a href='https://github.com/pxseu' target='_blank'>
+				Github
+			</a>
 		</p>
 		<p>
-			<a href='https://twitter.com/pxseu'>Twitter</a>
+			<a href='https://twitter.com/pxseu' target='_blank'>
+				Twitter
+			</a>
 		</p>
 		<p>
-			<a href='mailto:me@pxseu.com'>Email</a>
+			<a href='mailto:me@pxseu.com' target='_blank'>
+				Email
+			</a>
 		</p>
 		<p>
-			<a href='https://discord.pxseu.com'>Discord</a>
+			<a href='https://discord.pxseu.com' target='_blank'>
+				Discord
+			</a>
 		</p>
 	</DefaultLayout>
 );
