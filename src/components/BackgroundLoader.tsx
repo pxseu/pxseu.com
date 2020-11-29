4d2005cf4fdfc5d4a263d566243e168459b79369
pxseu <57842793+pxseu@users.noreply.github.com>
2020-11-29 23:50:29 +0100

    s

--- src/components/BackgroundLoader.tsx
+++ src/components/BackgroundLoader.tsx
@@ -37,7 +37,7 @@ const BackgroundLoader = (props: { children: any }) => {
 			"I love you <3",
 			"(⁄˘⁄ ⁄ ω⁄ ⁄ ˘⁄)",
 		],
-		DarlingCode = ["p", "e", "i", "t", "h", "o"];
+		DarlingCode = ["i", "a", "m", "r", "e", "t", "a", "r", "d", "e", "d"];
 
 	const loop = () => {
 		const titleEl = document.getElementsByTagName("title")[0];
