37942dc04ddb6cf2f5c2aa3ff879f024ba1e02ce
pxseu <57842793+pxseu@users.noreply.github.com>
2020-11-30 00:14:01 +0100

    s

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -5,17 +5,15 @@ const About = () => {
 		<>
 			<DefaultLayout title={"About me"}>
 				<p>Web and Back End developer</p>
-				<p>Always up for a new challenge</p>
 				<p>I use these languages:</p>
-				<p>C++, C#, TS, JS, Python</p>
+				<p>TS, JS</p>
 				<p>I have experience with:</p>
 				<p>
-					Next.js, React, Express, MongoDB, Discord.js, EJS, Passport,
-					Browser Extensions, Cross Platform Apps, Websockets, REST
-					Api's
+					Next.js, React, Express, MongoDB, Discord.js, EJS,
+					Websockets, REST Api's
 				</p>
 				<p>Currently learning / getting better with:</p>
-				<p>React and Next.js with Express</p>
+				<p>Nothing</p>
 			</DefaultLayout>
 		</>
 	);
