4f1978d82df2c90e42df313d11894ff522769132
pxseu <57842793+pxseu@users.noreply.github.com>
2020-12-06 22:41:36 +0100

    uwu

--- server/db/models/auth_key.ts
+++ server/db/models/auth_key.ts
@@ -1,5 +1,10 @@
-import mongoose from "mongoose";
+import mongoose, { Document } from "mongoose";
 const Schema = mongoose.Schema;
+
+export type apiUser = Document & {
+	auth_key: string;
+	user: string;
+};
 
 const serverSchema = new Schema({
 	auth_key: {
