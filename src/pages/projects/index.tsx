899ef62980d112e638c01c1d540de1745dd8553f
pxseu <57842793+pxseu@users.noreply.github.com>
2020-12-14 16:17:04 +0100

    47 changes aaaaaaaaaaaaaaaa

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -4,31 +4,41 @@ const Projects = () => (
 	<div>
 		<DefaultLayout title={"My Projects"}>
 			<p>
-				<a href='https://github.com/PreMiD/Discord-Bot'>
-					Little work on the PreMiD bot
-				</a>
+				<a href="https://github.com/PreMiD/Discord-Bot">Little work on the PreMiD bot</a>
 			</p>
 			<p>
-				<a href='https://short.pxseu.com/'>Url shrinker!</a>
+				<a href="https://short.pxseu.com/">Url shrinker!</a>
 			</p>
 			<p>
-				<a href='https://cdn.pxseu.com/'>File uploader!</a>
+				<a href="https://cdn.pxseu.com/">File uploader!</a>
 			</p>
 			<p>
-				<a href='https://chat.pxseu.com/'>Chat App!</a>
+				<a href="https://chat.pxseu.com/">Chat App!</a>
 			</p>
 			<p>
-				<a href='https://dash.pxseu.com/'>Dasboard for my apps!</a>
+				<a href="https://dash.pxseu.com/">Dasboard for my apps!</a>
 			</p>
 			<p>
-				<a href='https://github.com/pxseu/pxseu.com'>This website!</a>
+				<a href="https://github.com/pxseu/pxseu.com">This website!</a>
 			</p>
 			<p>
-				<a href='https://github.com/pxseu/ZeroTwoBot'>
-					My own Discord bot
-				</a>
+				<a href="https://github.com/pxseu/ZeroTwoBot">My own Discord bot</a>
+			</p>
+			<p className="comment">
+				<i>psst these are links!</i>
 			</p>
 		</DefaultLayout>
+		<style jsx>{`
+			.comment {
+				color: #fff;
+				text-decoration: underline #ffa9ff;
+				transition-duration: 0.3s;
+			}
+			.comment:hover {
+				color: #ffa9ff;
+				text-decoration: underline #fff;
+			}
+		`}</style>
 	</div>
 );
 
