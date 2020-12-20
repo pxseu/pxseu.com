d830848391cda5deac508ee4ac2b277062dad7bb
pxseu <57842793+pxseu@users.noreply.github.com>
2020-12-20 21:30:24 +0100

    uwu

--- src/pages/hall-of-fame/index.tsx
+++ src/pages/hall-of-fame/index.tsx
@@ -4,46 +4,51 @@ const HallOfFame = () => (
 	<>
 		<DefaultLayout title={"Hall of fame"}>
 			<p>
-				<a href='https://www.vektrix.com' target='_blank'>
+				<a href="https://www.vektrix.com" target="_blank">
 					Vek | told me about security.txt
 				</a>
 			</p>
 			<p>
-				<a href='https://twitter.com/milkeusz' target='_blank'>
+				<a href="https://twitter.com/milkeusz" target="_blank">
 					Milki | nothing tbh
 				</a>
 			</p>
 			<p>
-				<a href='https://twitter.com/Amuq16' target='_blank'>
+				<a href="https://twitter.com/rubinstagram" target="_blank">
+					Sylwia | gave her own opinion about this websites design
+				</a>
+			</p>
+			<p>
+				<a href="https://twitter.com/Amuq16" target="_blank">
 					Amuq | DDOS'ed my website
 				</a>
 			</p>
 			<p>
-				<a href='https://twitter.com/Apxllooo' target='_blank'>
+				<a href="https://twitter.com/Apxllooo" target="_blank">
 					Apx | sent minecraft mods
 				</a>
 			</p>
 			<p>
-				<a href='https://jelnislaw.ml/' target='_blank'>
+				<a href="https://jelnislaw.ml/" target="_blank">
 					JelNiSław | menel
 				</a>
 			</p>
 			<p>
-				<a href='https://twitter.com/crash7_' target='_blank'>
+				<a href="https://twitter.com/crash7_" target="_blank">
 					crash | przekonał mnie
 				</a>
 			</p>
 			<p>
-				<a href='https://mev.pxseu.com' target='_blank'>
+				<a href="https://mev.pxseu.com" target="_blank">
 					Mev | co-founder of @booble-site
 				</a>
 			</p>
 			<p>
-				<a href='https://RedBird16.com' target='_blank'>
+				<a href="https://RedBird16.com" target="_blank">
 					Red_Prez16 | coder @ RedBird16.com
 				</a>
 			</p>
-			<p className='comment'>
+			<p className="comment">
 				<i>psst these are links!</i>
 			</p>
 		</DefaultLayout>
