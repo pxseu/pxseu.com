62d758a754b4bfbb019f78cdb0015a63fcd0b983
pxseu <57842793+pxseu@users.noreply.github.com>
2020-12-28 14:25:06 +0100

    better 404

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,19 +11,11 @@ class MyDocument extends Document {
 			<>
 				<Html lang="en">
 					<Head />
-					{<html dangerouslySetInnerHTML={{ __html: "<!-- uwu -->" }}></html>}
 					<body>
-						{
-							<noscript
-								dangerouslySetInnerHTML={{
-									__html: `<p id="___nojs">UwU Please turn on javascript!<br />Thanks! -pxseu</p>
-								<style>
-									#__next {display: none;}
-									#___nojs {position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); font-size: 30px; text-align: center;}
-									body {color: white; background-color: black; font-family: Arial, Helvetica, sans-serif;}
-								</style>`,
-								}}></noscript>
-						}
+						<noscript
+							dangerouslySetInnerHTML={{
+								__html: `<p id="___nojs">UwU Please turn on javascript!<br />Thanks! -pxseu</p><style>#__next {display: none;}#___nojs {position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); font-size: 30px; text-align: center;}body {color: white; background-color: black; font-family: Arial, Helvetica, sans-serif;}</style>`,
+							}}></noscript>
 						<Main />
 						<NextScript />
 					</body>
