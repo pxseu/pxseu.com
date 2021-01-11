69ca777c85d3dbca30c64c38040744ba40e0df3c
pxseu <57842793+pxseu@users.noreply.github.com>
2021-01-11 14:01:12 +0100

    i thin this was the last inline jsx css aaaaaaaa

--- src/components/BackgroundLoader.tsx
+++ src/components/BackgroundLoader.tsx
@@ -4,6 +4,7 @@ import Particles from "react-particles-js";
 import { isMobile } from "react-device-detect";
 import { useEffect, useState } from "react";
 import Modal from "./Modal";
+import styles from "../styles/components/BackgroundLoader.module.css";
 
 const particlesSwitchName = "particlesSwitch";
 
@@ -163,7 +164,7 @@ const BackgroundLoader = (props: { children: ReactNode }) => {
 				{/* Misc */}
 				<meta
 					name="viewport"
-					content="width=device-width, initial-scale=0.8, maximum-scale=0.8, minimum-scale=0.8, user-scalable=no, minimal-ui"
+					content="width=device-width, initial-scale=0.8, maximum-scale=0.8, minimum-scale=0.8"
 				/>
 				<meta
 					name="google-site-verification"
@@ -172,83 +173,18 @@ const BackgroundLoader = (props: { children: ReactNode }) => {
 				{/* End Misc */}
 			</Head>
 			{props.children}
-			<div className="particlesSwitch">
-				<label className="switch">
+			<div className={styles.particlesSwitch}>
+				<label className={styles.switch}>
 					<p className="visually_hidden noselect">Switch particles on or off</p>
 					<input
 						type="checkbox"
-						id="switchLabel"
+						className={styles.switchInput}
 						checked={particlesSwitch}
 						onChange={() => switchPaticles()}
 					/>
-					<span className="slider round"></span>
+					<span className={`${styles.slider} ${styles.round}`}></span>
 				</label>
 			</div>
-			<style jsx>{`
-				.particlesSwitch {
-					position: fixed;
-					bottom: 0;
-					left: 0;
-					z-index: 5;
-					background-color: ${particlesSwitch
-						? "rgba(44, 44, 44, 0.2)"
-						: "rgba(0, 0, 0, 0.2)"};
-					width: 80px;
-					/* padding: 10px; */
-					border-radius: 20px;
-				}
-
-				.switch {
-					position: relative;
-					display: inline-block;
-					width: 60px;
-					height: 34px;
-				}
-				.switch input {
-					opacity: 0;
-					width: 0;
-					height: 0;
-				}
-				.slider {
-					position: absolute;
-					cursor: pointer;
-					top: 0;
-					left: 0;
-					right: 0;
-					bottom: 0;
-					background-color: #ccc;
-					-webkit-transition: 0.4s;
-					transition: 0.4s;
-				}
-				.slider:before {
-					position: absolute;
-					content: "";
-					height: 26px;
-					width: 26px;
-					left: 4px;
-					bottom: 4px;
-					background-color: white;
-					-webkit-transition: 0.4s;
-					transition: 0.4s;
-				}
-				input:checked + .slider {
-					background-color: #ffa9ff;
-				}
-				input:focus + .slider {
-					box-shadow: 0 0 1px #ffa9ff;
-				}
-				input:checked + .slider:before {
-					-webkit-transform: translateX(26px);
-					-ms-transform: translateX(26px);
-					transform: translateX(26px);
-				}
-				.slider.round {
-					border-radius: 34px;
-				}
-				.slider.round:before {
-					border-radius: 50%;
-				}
-			`}</style>
 			<PlatformParticles on={particlesSwitch} />
 			<Modal open={darling} onClose={() => setDarling(false)}>
 				I love you Darling!
