71f0f7f9160e923adc30c8c57abeb9e1b4229370
pxseu <57842793+pxseu@users.noreply.github.com>
2021-01-21 16:08:42 +0100

    new bradning for meeee

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,7 +6,7 @@ import Age from "../components/Age";
 import styles from "../styles/pages/About.module.css";
 
 const About = (): JSX.Element => (
-	<DefaultLayout title="About me!" image="/assets/svg/logo/logo-bg.svg" mediaType="image">
+	<DefaultLayout title="About me!" image="/images/pfp.png?v=3" mediaType="image">
 		<h2>
 			Hey <Twemoji emoji="👋" />
 		</h2>
