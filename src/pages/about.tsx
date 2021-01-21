ca0f4ce615fc68d19af2a5d9ad8978aadbd957d6
pxseu <57842793+pxseu@users.noreply.github.com>
2021-01-21 15:16:03 +0100

    rebranding yuh

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,7 +6,7 @@ import Age from "../components/Age";
 import styles from "../styles/pages/About.module.css";
 
 const About = (): JSX.Element => (
-	<DefaultLayout title="About me!" image="/images/pfp.png?v=2" mediaType="image">
+	<DefaultLayout title="About me!" image="/assets/svg/logo/logo-bg.svg" mediaType="image">
 		<h2>
 			Hey <Twemoji emoji="👋" />
 		</h2>
@@ -14,12 +14,11 @@ const About = (): JSX.Element => (
 			I&rsquo;m <span className={styles.me}>pxseu</span>!
 		</p>
 		<p>
-			I&rsquo;m a <Age timestamp={1090022400000 /* totaly not my bd 😳 */} /> year old
-			developer from Poland.
+			I&rsquo;m a <Age timestamp={1090022400000 /* totaly not my bd 😳 */} /> year old developer from Poland.
 		</p>
 		<p>I work in Frontend and Backend areas.</p>
 		<p>I enjoy making Api&rsquo;s, and other small scripts.</p>
-		<p>I use:</p>
+		<p>Stuff I use:</p>
 		<p>
 			<BadgeComp badge="typescript" />
 			<BadgeComp badge="javascript" />
