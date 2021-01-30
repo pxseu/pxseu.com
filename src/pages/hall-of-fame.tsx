7f3be03f4deab086b3a4cf6b6adefa7e6d9b425c
pxseu <57842793+pxseu@users.noreply.github.com>
2021-01-31 00:12:11 +0100

    better fame

--- src/pages/hall-of-fame.tsx
+++ src/pages/hall-of-fame.tsx
@@ -3,60 +3,54 @@ import DefaultLayout from "../components/DefaultLayout";
 import Twemoji from "../components/Twemoji";
 import styles from "../styles/pages/Comment.module.css";
 
+const Spacer = (): JSX.Element => <span className={styles.spacer}>|</span>;
+
+interface PersonInHofProps {
+	url: string;
+	person: string;
+	desc: string;
+	emoji?: string;
+}
+
+const PersonInHof = ({ url, person, desc, emoji }: PersonInHofProps): JSX.Element => (
+	<p>
+		<a className="link" href={url} target="_blank" rel="noreferrer">
+			{person} <Spacer /> {desc} {emoji && <Twemoji emoji={emoji} />}
+		</a>
+	</p>
+);
+
 const HallOfFame = (): JSX.Element => (
 	<DefaultLayout title={"Hall of fame"}>
-		<p>
-			<a className="link" href="https://twitter.com/rubinstagram" target="_blank" rel="noreferrer">
-				Sylwia | gave her own opinion about this websites design <Twemoji emoji="💅" />
-			</a>
-		</p>
-		<p>
-			<a className="link" href="https://kawakaami.dev/" target="_blank" rel="noreferrer">
-				kawakami | most marvelous girl in the world <Twemoji emoji="🥺" />
-			</a>
-		</p>
-		<p>
-			<a className="link" href="https://mev.pxseu.com" target="_blank" rel="noreferrer">
-				Mev | co-founder of @booble-site <Twemoji emoji="🌐" />
-			</a>
-		</p>
+		<PersonInHof
+			person="Sylwia"
+			desc="gave her own opinion about this websites design"
+			url="https://twitter.com/rubinstagram"
+			emoji="💅"
+		/>
+		<PersonInHof
+			person="kawakami"
+			desc="most marvelous girl in the world"
+			url="https://kawakaami.dev/"
+			emoji="🥺"
+		/>
+		<PersonInHof
+			person="narpi"
+			desc="amazing Minecraft plugin developer"
+			url="https://web.archive.org/web/20191010183438/https://narpi.me/"
+			emoji="🎮"
+		/>
+		<PersonInHof person="naomi" desc="amazing coder and my cutie" url="https://kawakaami.dev/" emoji="❤" />
+		<PersonInHof person="Mev" desc="co-founder of @booble-site" url="https://mev.pxseu.com" emoji="🌐" />
 		{/* If you see this i don't want to ask you because it's past you
-		and yeah i don't want to make you unhappy
-		<p>
-			<a className="link" href="https://www.vektrix.cc" target="_blank" rel="noreferrer">
-				Vek | told me about security.txt
-			</a>
-		</p> */}
-		<p>
-			<a className="link" href="https://twitter.com/Amuq16" target="_blank" rel="noreferrer">
-				Amuq | DDOS&rsquo;ed my website <Twemoji emoji="💥" />
-			</a>
-		</p>
-		<p>
-			<a className="link" href="https://twitter.com/Apxllooo" target="_blank" rel="noreferrer">
-				Apx | sent minecraft mods <Twemoji emoji="🛠" />
-			</a>
-		</p>
-		<p>
-			<a className="link" href="https://twitter.com/crash7_" target="_blank" rel="noreferrer">
-				crash | przekonał mnie <Twemoji emoji="😳" />
-			</a>
-		</p>
-		<p>
-			<a className="link" href="https://twitter.com/milkeusz" target="_blank" rel="noreferrer">
-				Milki | nothing special <Twemoji emoji="😎" />
-			</a>
-		</p>
-		<p>
-			<a className="link" href="https://jelnislaw.ml/" target="_blank" rel="noreferrer">
-				JelNiSław | menel <Twemoji emoji="😁" />
-			</a>
-		</p>
-		<p>
-			<a className="link" href="https://ririxi.dev/" target="_blank" rel="noreferrer">
-				ririxi | cute furry <Twemoji emoji="😍" />
-			</a>
-		</p>
+		and yeah i don't want to make you unhappy 
+		<PersonInHof person="Vek" desc="told me about security.txt" url="https://www.vektrix.cc" emoji="🔐" /> */}
+		<PersonInHof person="Amuq" desc="DDOS'ed my website" url="https://twitter.com/Amuq16" emoji="💥" />
+		<PersonInHof person="Apx" desc="sent Minecraft mods" url="https://twitter.com/Apxllooo" emoji="🛠" />
+		<PersonInHof person="crash" desc="przekonał mnie" url="https://twitter.com/crash7_" emoji="😳" />
+		<PersonInHof person="Milki" desc="nothing special" url="https://twitter.com/milkeusz" emoji="😎" />
+		<PersonInHof person="JelNiSław" desc="menel" url="https://jelnislaw.ml/" emoji="😁" />
+		<PersonInHof person="ririxi" desc="cute furry" url="https://ririxi.dev/" emoji="😍" />
 		<p className={styles.comment}>
 			<i>psst these are links!</i>
 		</p>
