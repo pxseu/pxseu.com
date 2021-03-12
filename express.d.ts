8fd0c78c82193e3c497c63e3eae2450d7eecddb5
pxseu <57842793+pxseu@users.noreply.github.com>
2021-03-12 19:23:00 +0100

    changes

--- express.d.ts
+++ express.d.ts
@@ -1,5 +1,16 @@
-import { Request } from "express";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import type { Request, Response as IResponse } from "express";
+import type { apiUser } from "./server/db/models/auth_key";
 
 export interface RequestWithUser extends Request {
-	user?: string;
+	user?: apiUser;
+}
+
+declare global {
+	namespace Express {
+		interface Response {
+			api: (status: number, body: Record<string, any>) => IResponse<any, Record<string, any>>;
+		}
+	}
 }
---
+++ server/utils/config.ts
@@ -0,0 +1,39 @@
+export const rewrites = [
+	{
+		req: "/bio",
+		res: "/about",
+	},
+	{
+		req: "/msg",
+		res: "/other/message",
+	},
+	{
+		req: "/%F0%9F%92%97",
+		res: "https://ririxi.dev/",
+	},
+];
+
+export const cspDirectives = {
+	directives: {
+		defaultSrc: ["'self'"],
+		baseUri: ["'self'"],
+		fontSrc: ["'self'", "https:", "data:"],
+		frameAncestors: ["'self'"],
+		imgSrc: ["'self'", "data:", "cdn.pxseu.com", "www.thiswaifudoesnotexist.net", "twemoji.maxcdn.com"],
+		objectSrc: ["'none'"],
+		scriptSrc: ["'self'", "ajax.cloudflare.com", "static.cloudflareinsights.com"],
+		scriptSrcAttr: ["'none'"],
+		styleSrc: ["'self'", "https:", "'unsafe-inline'"],
+		connectSrc: [
+			"'self'",
+			"data:",
+			"cdn.pxseu.com",
+			"www.thiswaifudoesnotexist.net",
+			"twemoji.maxcdn.com",
+			"ajax.cloudflare.com",
+			"static.cloudflareinsights.com",
+		],
+		upgradeInsecureRequests: [],
+		blockAllMixedContent: [],
+	},
+};
