bd8791f9ef9e76c49689e402e2c7d90b0464c080
pxseu <57842793+pxseu@users.noreply.github.com>
2021-04-03 19:51:33 +0200

    forgot to add a title

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -6,22 +6,25 @@ import styles from "../styles/pages/Projects.module.css";
 
 const Projects = (): JSX.Element => (
 	<DefaultLayout title={"My Projects"}>
-		<div className={styles.projectsWrapper}>
-			<StyledLink href="//github.com/imperialbin/imperial-node">Imperial Node</StyledLink>
+		<div className="center">
+			<h3>Bellow are some projects I worked on!</h3>
+			<div className={styles.projectsWrapper}>
+				<StyledLink href="//github.com/imperialbin/imperial-node">Imperial Node</StyledLink>
 
-			<StyledLink href="//github.com/pxseu/pxseu.com">This website!</StyledLink>
+				<StyledLink href="//github.com/pxseu/pxseu.com">This website!</StyledLink>
 
-			<StyledLink href="//github.com/pxseu/ZeroTwoBot">My own Discord bot</StyledLink>
+				<StyledLink href="//github.com/pxseu/ZeroTwoBot">My own Discord bot</StyledLink>
 
-			<StyledLink href="//short.pxseu.com/">Url shrinker!</StyledLink>
+				<StyledLink href="//short.pxseu.com/">Url shrinker!</StyledLink>
 
-			<StyledLink href="//cdn.pxseu.com/">File uploader!</StyledLink>
+				<StyledLink href="//cdn.pxseu.com/">File uploader!</StyledLink>
 
-			<StyledLink href="//dash.pxseu.com/">Dasboard for my apps!</StyledLink>
+				<StyledLink href="//dash.pxseu.com/">Dasboard for my apps!</StyledLink>
 
-			<p className={commentStyles.comment}>
-				<i>psst these are links!</i>
-			</p>
+				<p className={commentStyles.comment}>
+					<i>psst these are links!</i>
+				</p>
+			</div>
 		</div>
 	</DefaultLayout>
 );
