adb4d8d0febcd01e946d71e2ee8e5cd1f6b9d136
pxseu <57842793+pxseu@users.noreply.github.com>
2021-04-04 12:10:15 +0200

    fuck u

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,35 +2,52 @@ import React, { useEffect, useRef, memo, ReactNode } from "react";
 import { createPortal } from "react-dom";
 import styles from "../styles/components/Modal.module.css";
 
-const Modal = ({
-	open,
-	onClose,
-	children,
-}: {
+interface ModalInteraface {
 	open: boolean;
+	title: string;
 	onClose: () => void | Promise<void>;
 	children: ReactNode;
-}) => {
+}
+
+const Modal = ({ open, onClose, children }: ModalInteraface) => {
 	if (!open) return null;
 	const ModalText = children == undefined ? "What?" : children;
 	const modalEl = useRef<HTMLDivElement>(null);
+
 	useEffect(() => {
-		setTimeout(() => modalEl.current?.classList.remove(styles.fade), 100);
-	});
+		const timeOut = setTimeout(() => modalEl.current?.classList.remove(styles.fade), 100);
+
+		document.addEventListener("keydown", handleEscape);
+
+		return () => {
+			clearTimeout(timeOut);
+			document.removeEventListener("keydown", handleEscape);
+		};
+	}, []);
+
+	function handleEscape(e: KeyboardEvent): void {
+		if (e.key === "Escape" || e.key === "Esc") {
+			close();
+		}
+	}
+
+	function close() {
+		modalEl.current?.classList.add(styles.fade);
+		setTimeout(() => onClose(), 400);
+	}
 
 	return createPortal(
 		<>
-			<div id="overlay" className={styles.overlay} />
+			<div id="overlay" className={styles.overlay} onClick={() => close()} />
 			<div id="modal" className={`${styles.modal} ${styles.fade} noselect`} ref={modalEl}>
+				<div id="closeModal" className={styles.closeModal} onClick={() => close()}></div>
 				<div
-					id="closeModal"
-					className={styles.closeModal}
+					id="modalQuestion"
+					className={styles.modalQuestion}
 					onClick={() => {
-						modalEl.current?.classList.add(styles.fade);
-						setTimeout(() => onClose(), 400);
-					}}>
-					x
-				</div>
+						if (window?.location?.href)
+							window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+					}}></div>
 				<div className={styles.modalContent}>{ModalText}</div>
 			</div>
 		</>,
