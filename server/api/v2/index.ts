8698b49cb400593914b890b351d486465a4e493a
pxseu <57842793+pxseu@users.noreply.github.com>
2021-04-08 22:23:01 +0200

    shit

--- server/api/v2/index.ts
+++ server/api/v2/index.ts
@@ -1,13 +1,12 @@
 import { Router } from "express";
-import { methodCheck } from "../methodCheck";
 import { bajoJajo } from "./bajoJajo";
-import { isValidMessage, sendMessage } from "./sendMessage";
-import spotify from "./spotify";
+import { router as sendMessageRouter } from "./sendMessage";
+import { router as spotifyRouter } from "./spotify";
 
 const router = Router();
 
-router.use("/sendMessage", methodCheck.post, isValidMessage, sendMessage);
-router.use("/bajoJajo", methodCheck.get, bajoJajo);
-router.use("/spotify", spotify);
+router.use("/sendMessage", sendMessageRouter);
+router.use("/bajoJajo", bajoJajo);
+router.use("/spotify", spotifyRouter);
 
 export default router;
---
+++ server/api/v2/sendMessage/index.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { postMessage, validateMessage } from "./sendMessagePost";
+import { MessageEmbed, WebhookClient, WebhookMessageOptions } from "discord.js";
+
+export const AVATAR = "https://cdn.pxseu.com/5As8jItIj.jpg";
+export const client = new WebhookClient(process.env.WEBHOOK_MESSAGE_ID ?? "", process.env.WEBHOOK_MESSAGE_TOKEN ?? "");
+
+export const embedWithBase = (embed: MessageEmbed): WebhookMessageOptions => {
+	return {
+		username: "anon chat",
+		avatarURL: AVATAR,
+		embeds: [embed],
+	};
+};
+
+export const router = Router();
+
+router.post("/", validateMessage, postMessage);
