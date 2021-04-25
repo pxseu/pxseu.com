a97e8fb7084fb392f60f95cfb7ed10a6184d43bd
pxseu <57842793+pxseu@users.noreply.github.com>
2021-04-25 13:36:10 +0200

    update skeleton

--- src/components/TopSongs.tsx
+++ src/components/TopSongs.tsx
@@ -7,13 +7,13 @@ import styles from "../styles/components/TopSongs.module.css";
 interface Song {
 	song: {
 		name: string;
-		artists: string;
-		url: string;
+		artists?: string;
+		url?: string;
 	};
 	album: {
-		name: string;
-		image: string;
-		url: string;
+		name?: string;
+		image?: string;
+		url?: string;
 	};
 }
 
@@ -29,12 +29,17 @@ const TopSongs = (): JSX.Element => {
 					{emtpyArr.map((_, key) => {
 						return (
 							<div className={styles.song} key={key}>
-								<div className={styles.number}>{key + 1}</div>
+								<div className={styles.songImage}>
+									<Skeleton width={100} height={100} />
+								</div>
 								<div className={styles.songContent}>
 									<div className={styles.songTitle}>
 										<Skeleton />
 									</div>
-									<div className={styles.songArtists}>
+									<div className={styles.artists}>
+										<Skeleton />
+									</div>
+									<div className={styles.album}>
 										<Skeleton />
 									</div>
 								</div>
@@ -48,29 +53,42 @@ const TopSongs = (): JSX.Element => {
 
 	const tracks = data.tracks as Song[];
 
+	const placeholder = "/assets/placeholders/album.png";
+
 	return (
 		<div className={styles.topSongsWrapper}>
 			{tracks.map((track, key) => {
-				
+				const image = !track.album.image ? placeholder : track.album.image;
+				const artist = !track.song.artists ? "Unknown artist" : track.song.artists;
+				const album = !track.album.name ? "No album" : track.album.name;
+
 				return (
 					<div className={styles.song} key={key}>
 						<div className={styles.songImage}>
-							<img src={track.album.image} className={styles.songImgTag} />
+							<img src={image} className={styles.songImgTag} />
 						</div>
 						<div className={styles.songContent}>
 							<div className={styles.songTitle}>
-								<a href={track.song.url} title={track.song.name}>
-									{track.song.name}
-								</a>
+								{track.song.url ? (
+									<a href={track.song.url} title={track.song.name}>
+										{track.song.name}
+									</a>
+								) : (
+									<span title={track.song.name}>{track.song.name}</span>
+								)}
 							</div>
 							<div className={styles.artists}>
-								by <span title={track.song.artists}>{track.song.artists}</span>
+								by <span title={artist}>{artist}</span>
 							</div>
 							<div className={styles.album}>
 								on{" "}
-								<a href={track.album.url} title={track.album.name}>
-									{track.album.name}
-								</a>
+								{track.album.url ? (
+									<a href={track.album.url} title={album}>
+										{album}
+									</a>
+								) : (
+									<span title={album}>{album}</span>
+								)}
 							</div>
 						</div>
 					</div>
