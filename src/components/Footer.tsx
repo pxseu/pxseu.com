a40aa7fc7507ba142cd0cd5c06117d440aff7ca7
pxseu <57842793+pxseu@users.noreply.github.com>
2021-06-13 19:30:59 +0200

    feat: clear repo

--- src/components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "../styles/components/Footer.module.css";
-
-const Footer = (): JSX.Element => {
-	return <footer className={styles.footer}>&copy; pxseu 2021</footer>;
-};
-
-export default Footer;
--- src/lib/fetcher.ts
+++
@@ -1,4 +0,0 @@
-export const fetcher = async (input: RequestInfo, init?: RequestInit): Promise<Record<string, unknown>> => {
-	const res = await fetch(input, init);
-	return res.json();
-};
