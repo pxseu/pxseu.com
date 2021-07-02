5c624961db15ef5f91d2cbd7a7053a0ad88d3287
pxseu <57842793+pxseu@users.noreply.github.com>
2021-07-02 14:46:48 +0200

    chore: semi working ci

--- packages/api/src/routes/index.ts
+++ packages/api/src/routes/index.ts
@@ -5,6 +5,12 @@ export const router = Router();
 
 router.use("/v2", v2);
 
+router.get("/health", (_, res) => {
+	res.api(200, {
+		message: "OK",
+	});
+});
+
 router.use((_, res) => {
 	res.api(404, {
 		message: "Not Found",
