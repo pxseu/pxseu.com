fb4b4af5a1e7444a09295d1c68de5258ebb2a5b2
pxseu <57842793+pxseu@users.noreply.github.com>
2021-07-03 15:11:39 +0200

    chore: secrets in docker

--- packages/api/src/config.ts
+++ packages/api/src/config.ts
@@ -1,3 +1,4 @@
+export const { ENV_PATH } = process.env;
 export const HOSTNAME = process.env.HOSTNAME ?? "no_docker_hostname";
 export const API_HOSTNAME = process.env.API_HOSTNAME?.toLowerCase() ?? "api.pxseu.com";
 export const DEV = process.env.NODE_ENV !== "production";
--- packages/api/src/utils/env.ts
+++ packages/api/src/utils/env.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import dotenv from "dotenv";
+import { ENV_PATH } from "../config";
 
-const envPath = path.resolve(process.cwd(), `.env${process.env.NODE_ENV !== "production" ? ".local" : ""}`);
+const envPath = ENV_PATH ?? path.resolve(process.cwd(), `.env${process.env.NODE_ENV !== "production" ? ".local" : ""}`);
 
 dotenv.config({ path: envPath });
