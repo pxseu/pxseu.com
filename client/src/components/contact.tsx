32bb5719936077ea496e196e87c468ef6b5a2a06
pxseu <57842793+pxseu@users.noreply.github.com>
2025-04-08 12:06:59 +0200

    remove roundness

--- client/src/components/contact.tsx
+++ client/src/components/contact.tsx
@@ -17,7 +17,7 @@ export default function Contact({ items }: ContactsProps): JSX.Element {
 				<a
 					key={item.label}
 					href={item.link}
-					className="flex flex-col items-start border border-zinc-700 p-4 rounded shadow-sm text-zinc-100 hover:shadow-md transition"
+					className="flex flex-col items-start border border-zinc-700 p-4 shadow-sm text-zinc-100 hover:shadow-md transition"
 					target="_blank"
 					rel="noopener noreferrer"
 				>
