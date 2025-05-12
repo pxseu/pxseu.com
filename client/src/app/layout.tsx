d93bd4d540ea2ee49517502d5ce43aa06e83a178
pxseu <57842793+pxseu@users.noreply.github.com>
2025-05-12 22:47:06 +0200

    og meta

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -6,10 +6,57 @@ import { RealtimeProvider } from "@/contexts/RealtimeContext";
 import ReactLenis from "lenis/react";
 import { API_ROUTE } from "@/config";
 
+const THEME_COLOR = "#8066F7";
+const ASSET_VERSION = "3.0";
+const description = "pxseu's personal website";
+
 export const metadata: Metadata = {
 	title: "pxseu.com",
-	keywords: ["pxseu"],
+	description,
+	keywords: ["pxseu", "poseuxck"],
 	authors: [{ name: "pxseu", url: "https://pxseu.com" }],
+	robots: "index, follow",
+	manifest: `/manifest.json?v=${ASSET_VERSION}`,
+	applicationName: "pxseu",
+	appleWebApp: {
+		title: "pxseu",
+	},
+	icons: {
+		icon: [
+			{ url: `/favicon-16x16.png?v=${ASSET_VERSION}`, sizes: "16x16", type: "image/png" },
+			{ url: `/favicon-32x32.png?v=${ASSET_VERSION}`, sizes: "32x32", type: "image/png" },
+		],
+		shortcut: `/favicon.ico?v=${ASSET_VERSION}`,
+		apple: { url: `/apple-touch-icon.png?v=${ASSET_VERSION}`, sizes: "180x180", type: "image/png" },
+		other: [{ url: `/safari-pinned-tab.svg?v=${ASSET_VERSION}`, rel: "mask-icon", color: THEME_COLOR }],
+	},
+	themeColor: THEME_COLOR,
+	openGraph: {
+		url: "https://pxseu.com",
+		type: "website",
+		locale: "en_US",
+		siteName: "pxseu.com",
+		title: "pxseu.com",
+		images: [
+			{
+				url: `https://pxseu.com/android-chrome-512x512.png?v=${ASSET_VERSION}`,
+			},
+		],
+		description,
+	},
+	twitter: {
+		card: "summary_large_image",
+		site: "@pxseu",
+		title: "pxseu.com",
+		description,
+		images: [`https://pxseu.com/android-chrome-512x512.png?v=${ASSET_VERSION}`],
+	},
+	verification: {
+		google: "azPHAHBpTuJ-8stcPE_LX6-GNwVGjzp5_V7E3KCcmMk",
+	},
+	other: {
+		"msapplication-TileColor": THEME_COLOR,
+	},
 };
 
 export default function RootLayout({
