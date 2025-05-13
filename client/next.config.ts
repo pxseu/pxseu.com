418638c8ced4e7e1c69ed1f88b5bb264b1b62996
pxseu <57842793+pxseu@users.noreply.github.com>
2025-05-13 02:15:05 +0200

    react compiler

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	/* config options here */
+	experimental: {
+		reactCompiler: true,
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	typescript: {
+		ignoreBuildErrors: true,
+	},
 };
 
 export default nextConfig;
