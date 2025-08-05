59fc18b4717157695cce5ecee6bd83a15002316a
pxseu <57842793+pxseu@users.noreply.github.com>
2025-08-05 14:01:53 +0200

    try to load first

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -87,23 +87,25 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<head>
-				<CSideScript />
-				<link rel="preconnect" href={API_ROUTE} />
-				<link rel="preconnect" href="https://i.scdn.co" />
-			</head>
-			<body className="flex justify-center bg-zinc-950 text-zinc-400 h-full p-6">
-				<div className="flex justify-center items-center max-w-[900px] w-full flex-col">
-					<RealtimeProvider>
-						<ReactLenis root options={{ smoothWheel: true, lerp: 0.2 }}>
-							<Header />
-							{children}
-							<Footer />
-						</ReactLenis>
-					</RealtimeProvider>
-				</div>
-			</body>
-		</html>
+		<>
+			<CSideScript />
+			<html lang="en">
+				<head>
+					<link rel="preconnect" href={API_ROUTE} />
+					<link rel="preconnect" href="https://i.scdn.co" />
+				</head>
+				<body className="flex justify-center bg-zinc-950 text-zinc-400 h-full p-6">
+					<div className="flex justify-center items-center max-w-[900px] w-full flex-col">
+						<RealtimeProvider>
+							<ReactLenis root options={{ smoothWheel: true, lerp: 0.2 }}>
+								<Header />
+								{children}
+								<Footer />
+							</ReactLenis>
+						</RealtimeProvider>
+					</div>
+				</body>
+			</html>
+		</>
 	);
 }
