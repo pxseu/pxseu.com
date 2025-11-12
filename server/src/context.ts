3020924df11a004dd3ab67897f6da5b20e0e95e6
pxseu <57842793+pxseu@users.noreply.github.com>
2025-11-12 07:11:43 +0100

    slop

--- server/src/context.ts
+++ server/src/context.ts
@@ -1,4 +1,4 @@
-import { createContext } from "@kaito-http/core";
+import { createUtilities } from "@kaito-http/core";
 import { createClients } from "./clients/index.js";
 import { ipStore } from "./index.js";
 import { realtimeManager } from "./realtime/index.js";
@@ -9,7 +9,7 @@ const serverStarted = Date.now();
 
 export const realtime = await realtimeManager(clients.redis);
 
-export const { getContext, router } = createContext(async (req) => {
+export const { getContext, router } = createUtilities(async (req) => {
 	const ip =
 		req.headers.get("x-forwarded-for") ?? ipStore.getStore() ?? "unknown";
 
